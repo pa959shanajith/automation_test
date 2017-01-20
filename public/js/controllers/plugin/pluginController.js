be54ed422ba5d200e77eb33e334c89d0a150ca54
jatin.sharma <jatin.sharma@WSLK13FODC6-042.SLKSOFT.COM>
2017-01-20 10:42:37 +0530

    Changes in Task JSON

--- public/js/controllers/plugin/pluginController.js
+++ public/js/controllers/plugin/pluginController.js
@@ -8,10 +8,12 @@ mySPA.controller('pluginController',['$scope','$http','$location','$timeout','Pl
     $(document).on("click", ".panel-body", function(){
         window.location.href = '/design';
     })
-    $("#plugin-container").empty()
-    for(i=0; i<availablePlugins.length; i++){
-        $("#plugin-container").append('<div class="col-md-4 plugin-block"><span onclick="p_event(this.dataset.name)" data-name="p_'+availablePlugins[i].pluginName.replace(/\s/g,'')+'" id="'+availablePlugins[i].id+'">'+availablePlugins[i].pluginName+'</span></div>')
-    }
+    $("#plugin-container").empty().hide()
+    $timeout(function(){
+    	for(i=0; i<availablePlugins.length; i++){
+            $("#plugin-container").append('<div class="col-md-4 plugin-block"><span onclick="p_event(this.dataset.name)" data-name="p_'+availablePlugins[i].pluginName.replace(/\s/g,'')+'" id="'+availablePlugins[i].id+'">'+availablePlugins[i].pluginName+'</span></div>').fadeIn()
+        }
+    }, 200)
 
     //Plugin click event 
     $scope.pluginFunction = function(name){
@@ -24,48 +26,55 @@ mySPA.controller('pluginController',['$scope','$http','$location','$timeout','Pl
     	"Node_Type": "Web",
     	"Node_Id": "4464668464",
     	"Task_Type": "Design",
+    	"Task_Name": "Create Screen",
     	"Release_Id": "68744446+",
     	"Cycle_Id": "574644446"
     }, {
     	"Project_Id": "4464668464",
-    	"Node_Type": "Web",
+    	"Node_Type": "Desktop",
     	"Node_Id": "4464668464",
     	"Task_Type": "Execution",
+    	"Task_Name": "Create TestSuite",
     	"Release_Id": "68744446+",
     	"Cycle_Id": "574644446"
     }, {
     	"Project_Id": "4464668464",
-    	"Node_Type": "Web",
+    	"Node_Type": "iOS",
     	"Node_Id": "4464668464",
     	"Task_Type": "Design",
+    	"Task_Name": "Create TestScript",
     	"Release_Id": "68744446+",
     	"Cycle_Id": "574644446"
     }, {
     	"Project_Id": "4464668464",
-    	"Node_Type": "Web",
+    	"Node_Type": "Android",
     	"Node_Id": "4464668464",
     	"Task_Type": "Design",
+    	"Task_Name": "Debug TestScript",
     	"Release_Id": "68744446+",
     	"Cycle_Id": "574644446"
     }, {
     	"Project_Id": "4464668464",
-    	"Node_Type": "Web",
+    	"Node_Type": "DesktopJava",
     	"Node_Id": "4464668464",
     	"Task_Type": "Execution",
+    	"Task_Name": "Schedule TestSuite",
     	"Release_Id": "68744446+",
     	"Cycle_Id": "574644446"
     }]
-    $(".plugin-taks-listing").empty()
+    $(".plugin-taks-listing").empty().hide()
+    var counter = 1;
     $timeout(function(){
     	for(i=0; i<tasksJson.length; i++){
-	    	if(tasksJson[i].Task_Type == "Design"){
-	    		$(".plugin-taks-listing").append('<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><div class="collapse-head" data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'"><span class="taskNo">Task '+ i +'</span><!--Addition--><div class="panel-additional-details"><span class="panel-head-tasktype">'+tasksJson[i].Task_Type+'</span><span class="panel-head-details details-design-task">Details <span class="caret caret-absolute"></span></span></div><!--Addition--></div></h4></div><div id="collapse'+i+'" class="panel-collapse collapse"><div class="panel-body"><span class="assignedTask"></span></div></div></div>')
+    		if(tasksJson[i].Task_Type == "Design"){
+	    		$(".plugin-taks-listing").append('<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><div class="collapse-head" data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'"><span class="taskNo">Task '+ counter +'</span><!--Addition--><div class="panel-additional-details"><span class="panel-head-tasktype">'+tasksJson[i].Task_Type+'</span><span class="panel-head-details details-design-task">Details <span class="caret caret-absolute"></span></span></div><!--Addition--></div></h4></div><div id="collapse'+i+'" class="panel-collapse collapse"><div class="panel-body"><span class="assignedTask">'+tasksJson[i].Task_Name+'</span></div></div></div>').fadeIn()
 			} 
 	    	else if(tasksJson[i].Task_Type == "Execution"){
-	    		$(".plugin-taks-listing").append('<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><div class="collapse-head" data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'"><span class="taskNo">Task '+ i +'</span><!--Addition--><div class="panel-additional-details"><span class="panel-head-tasktype">'+tasksJson[i].Task_Type+'</span><span class="panel-head-details details-execute-task">Details <span class="caret caret-absolute"></span></span></div><!--Addition--></div></h4></div><div id="collapse'+i+'" class="panel-collapse collapse"><div class="panel-body"><span class="assignedTask"></span></div></div></div>')
+	    		$(".plugin-taks-listing").append('<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><div class="collapse-head" data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'"><span class="taskNo">Task '+ counter +'</span><!--Addition--><div class="panel-additional-details"><span class="panel-head-tasktype">'+tasksJson[i].Task_Type+'</span><span class="panel-head-details details-execute-task">Details <span class="caret caret-absolute"></span></span></div><!--Addition--></div></h4></div><div id="collapse'+i+'" class="panel-collapse collapse"><div class="panel-body"><span class="assignedTask">'+tasksJson[i].Task_Name+'</span></div></div></div>').fadeIn()
 	    	}
+    		counter++
 	    }
-    }, 500)
+    }, 200)
 }]);
 
 //Plugin click event - Creating Scope to define the function and returning back to controller
