23e3eaf887ff2b89000e8e56b5248bf84ec1fcdd
vinay.niranjan <vinay.niranjan@WSLK13FODC6-028.SLKSOFT.COM>
2017-01-24 15:52:56 +0530

    Design Screen - design change

--- public/js/controllers/design/designController.js
+++ public/js/controllers/design/designController.js
@@ -21,7 +21,7 @@ mySPA.controller('designController', ['$scope', '$http', '$location', '$timeout'
         viewString.view = data;
         console.log("Scrapping Started::::::")
         $("#finalScrap").empty()
-        $("#finalScrap").append("<div id='scrapTree' class='scrapTree'><ul><li><input title='Select all' type='checkbox' class='checkStylebox'><a id='aScrapper'>Screen Fields </a><ul id='scraplist' class='scraplistStyle'></ul></li></ul></div>");
+        $("#finalScrap").append("<div id='scrapTree' class='scrapTree'><ul><li><span class='parentObjContainer'><input title='Select all' type='checkbox' class='checkStylebox'><span class='parentObject'><a id='aScrapper'>Select all </a><button class='btn btn-default objBtn'>Save</button><button class='btn btn-default objBtn'>Delete</button></span><span></span></span><ul id='scraplist' class='scraplistStyle'></ul></li></ul></div>");
         var custname;
 		var imgTag;
 		var scrapTree = $("#finalScrap").children('#scrapTree');
@@ -33,7 +33,7 @@ mySPA.controller('designController', ['$scope', '$http', '$location', '$timeout'
 			custname = ob.custname;
 			var tag = ob.tag;
 			//Add This after Anchor Tag in next line <input type='checkbox' name='selectAllListItems' />
-			var li = "<li class='item' val="+ob.tempId+"><input type='checkbox' class='checkall' name='selectAllListItems' /><a><img class='tag-icon' src='imgs/ic_"+imgTag+"_32x32.png'/>"+custname+"</a></li>";
+			var li = "<li class='item' val="+ob.tempId+"><span class='dtreeObjects'><img id='focus_"+i+"' class='focus-icon' src='imgs/ic-highlight-element-inactive.png'/><input type='checkbox' class='checkall' name='selectAllListItems' /><a><img class='tag-icon' src='imgs/ic_"+imgTag+"_32x32.png'/><span class='objectText'>"+custname+"</span></a></span></li>";
 			angular.element(innerUL).append(li)
 		}
         $(document).find('#scrapTree').scrapTree({
@@ -43,6 +43,12 @@ mySPA.controller('designController', ['$scope', '$http', '$location', '$timeout'
 			},
 			editable: true,
 		});
+        $(document).on('click', '.focus-icon', function(e) {
+        	var id = $(this).attr('id');
+        	$("img.focus-icon").removeAttr('src').attr('src','imgs/ic-highlight-element-inactive.png').removeClass('focus-highlight');
+        	$("#"+id).addClass('focus-highlight');
+        	$(".focus-highlight").removeAttr('src').attr('src','imgs/ic-highlight-element-active.png');
+        })
  	   }, function (error) { console.log("Fail to Load design_ICE") });
     }
 }]);
