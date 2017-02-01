94c952d455af48bc5b01f0be53fe76b9a12e5c77
vinay.niranjan <vinay.niranjan@WSLK13FODC6-028.SLKSOFT.COM>
2017-02-01 21:09:19 +0530

    Browser Highlight UI Implementation

--- server/dao/designDAO.js
+++ server/dao/designDAO.js
@@ -25,13 +25,35 @@ initScraping_ICE: function initScraping_ICE(req, cb, data) {
         {
            var data = "OPEN BROWSER IE";
         }
-       
+        else if(browserType == "mozilla")
+        {
+            var data = "OPEN BROWSER FX";
+        }
         var ip = req.headers['x-forwarded-for'] || req.info.remoteAddress;
         var mySocket =  myserver.allSocketsMap[ip];
         mySocket.send(data);
         mySocket.on('message', function(data){
    	        cb(null, data);
        });
+},
+
+highlightScrapElement_ICE: function highlightScrapElement_ICE(req, cb, data) {
+	var focusParam = req.payload.elementXpath+","+req.payload.elementUrl;
+	  var ip = req.headers['x-forwarded-for'] || req.info.remoteAddress;
+	  
+	  var mySocket =  myserver.allSocketsMap[ip];
+	  mySocket.emit("focus", focusParam);
+	  var flag = 'success';
+	  cb(null, flag);
+//	  console.log("-----------------------------------------")
+//	  //console.log(focusParam);
+//	  console.log("-----------------------------------------")
+//	  mySocket.on('highlight', function(data){
+//		  console.log("HIGHLIGHT FEATURE");
+//		  console.log("res", data);
+// 	        cb(null, data);
+//     });
+	  //callback success on highlight
 },
 
 deleteScrapeObjects_ICE: function deleteScrapeObjects_ICE(req, cb, data) {
