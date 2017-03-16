0c5a21688a9c7ea40e9cef5a88d815f0d639dafa
shree.p <shree.p@slksoft.com>
2017-03-16 17:33:01 +0530

    Removed unwanted check for test suite id in update test suite service.

--- server/controllers/suite.js
+++ server/controllers/suite.js
@@ -224,35 +224,27 @@ exports.updateTestSuite_ICE = function (req, res) {
 	var checksuiteexists = "";
 	var deleteTestSuiteQuery = "";
 	var updateTestSuiteData = "";
-			checksuiteexists = "select testsuiteid from testsuites where cycleid=" + requestedtestscycleid;
-			dbConnICE.execute(checksuiteexists, function (err, checksuiteexistsresult) {
-				if (err) {
-					var flag = "Error in Query 1 checksuiteexists: Fail";
-					//res.send(flag);
-				} else {
-					if (checksuiteexistsresult.rows[0].testsuiteid == requestedtestsuiteid) {
-						deleteTestSuiteQuery = "DELETE conditioncheck,donotexecute,getparampaths,testscenarioids FROM testsuites " +
-						"where cycleid=" + requestedtestscycleid +
-						" and testsuitename='" + requestedtestsuitename + "'" +
-						" and testsuiteid=" + requestedtestsuiteid +" and versionnumber = "+requestedversionnumber;
-						deleteSuite(deleteTestSuiteQuery,function(err,response){
-							if(response == "success"){
-								saveSuite(function(err,response){
-									if(err){
-										flag = "fail";
-										index=index+1;
-										if(index == requestedtestscenarioids.length){res.send(flag);}
-									}else{
-										flag = "success";
-										index=index+1;
-										if(index == requestedtestscenarioids.length){res.send(flag);}										
-									}
-								});
-							}
-						});
+		//Removed the unwanted check for testsuite id based on cycleid in testsuite table 
+		deleteTestSuiteQuery = "DELETE conditioncheck,donotexecute,getparampaths,testscenarioids FROM testsuites " +
+		"where cycleid=" + requestedtestscycleid +
+		" and testsuitename='" + requestedtestsuitename + "'" +
+		" and testsuiteid=" + requestedtestsuiteid +" and versionnumber = "+requestedversionnumber;
+		deleteSuite(deleteTestSuiteQuery,function(err,response){
+			if(response == "success"){
+				saveSuite(function(err,response){
+					if(err){
+						flag = "fail";
+						index=index+1;
+						if(index == requestedtestscenarioids.length){res.send(flag);}
+					}else{
+						flag = "success";
+						index=index+1;
+						if(index == requestedtestscenarioids.length){res.send(flag);}										
 					}
-				}
-			});
+				});
+			}
+		});
+
 
 	function deleteSuite(deleteTestSuiteQuery,deleteSuitecallback){
 		dbConnICE.execute(deleteTestSuiteQuery, function (err, deleteQueryresults) {
