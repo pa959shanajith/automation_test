6e20375d7366a75cb07bd4fea88edda6644177a1
vinay.niranjan <vinay.niranjan@WSLK13FODC6-042.SLKSOFT.COM>
2017-07-28 20:08:34 +0530

    #941:UI - Require Single place to confiure the server, currently there are multiple places to configure the server fixed

--- server/config/icetestautomation.js
+++ server/config/icetestautomation.js
@@ -1,8 +1,11 @@
+
+var options = require('./options');
 var cassandra = require('cassandra-driver');
-const authProvider = new cassandra.auth.PlainTextAuthProvider('nineteen68', 'TA@SLK2017');
+const authProvider = new cassandra.auth.PlainTextAuthProvider(options.storageConfig.username, options.storageConfig.password);
 module.exports = new cassandra.Client({
-    contactPoints: ['10.44.10.55'], 
+    contactPoints: [options.storageConfig.host], 
     keyspace: 'icetestautomation',
     authProvider: authProvider
 });
 console.log('Keyspace icetestautomation will be enabled.');  
+
