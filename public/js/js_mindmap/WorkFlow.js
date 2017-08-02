15c23c28abae8c8c6098a2f0151fcf57ed25a7ff
sushma.p <sushma.p@WSLK13FODC6-037.SLKSOFT.COM>
2017-08-02 10:51:19 +0530

    Logic written to update cassandra_ids (id_c) of nodes in dNodes_W variable as soon as the structure is created

--- public/js/js_mindmap/WorkFlow.js
+++ public/js/js_mindmap/WorkFlow.js
@@ -808,6 +808,12 @@ if(flag==20){
 					});
 			});
 		});
+		//To update cassandra_ids (id_c) of nodes in dNodes_W variable
+		dNodes_W.forEach(function(d){
+			if (d.type=='modules') d.id_c=res[resMap[0]];
+			else d.id_c=res[d.id_n];
+
+		});
 		
 		openDialogMindmap("Success", "Structure created successfully");
 		saveFlag=false;
