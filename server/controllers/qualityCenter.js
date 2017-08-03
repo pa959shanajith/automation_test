0335c6504bcdc669f7a10a709d226a637ca485cd
shree.p <shree.p@slksoft.com>
2017-08-03 20:13:32 +0530

    Service implementation for Quality center. - feature implementation

--- server/controllers/qualityCenter.js
+++ server/controllers/qualityCenter.js
@@ -8,6 +8,9 @@ var myserver = require('../../server.js');
 var uuid = require('uuid-random');
 var dbConnICE = require('../../server/config/icetestautomation');
 var dbConnICEHistory = require('../../server/config/ICEHistory');
+var epurl="http://127.0.0.1:1990/";
+var Client = require("node-rest-client").Client;
+var client = new Client();
 
 exports.loginQCServer_ICE = function (req, res) {
 	try{
@@ -115,9 +118,19 @@ function getProjectsForUser(userid,cb){
     async.series({
         getprojectDetails:function(callback1){
             var getprojects = "select projectids from icepermissions where userid="+userid;
-            dbConnICE.execute(getprojects,function(err,projectrows){
-                if(err){
-                    console.log(err);
+            var inputs = {"userid":userid,"query":"getprojectDetails"}
+            var args = {
+                data:inputs,
+                headers:{"Content-Type" : "application/json"}
+                
+            }
+            client.post(epurl+"qualityCenter/qcProjectDetails_ICE",args,
+                function (projectrows, response) {
+                    if (response.statusCode != 200 || projectrows.rows == "fail") {
+           // dbConnICE.execute(getprojects,function(err,projectrows){
+                //if(err){
+                    //console.log(err);
+                    console.log("Error occured in getProjectsForUser: fail , getprojectDetails");
                 }else{
                     if(projectrows.rows.length!=0){
                         flagtocheckifexists = true;
@@ -152,9 +165,19 @@ function projectandscenario(projectid,cb){
     async.series({
         projectname1 : function(callback1){
             var projectnamequery = "SELECT projectname FROM projects WHERE projectid="+projectid;
-            dbConnICE.execute(projectnamequery,function(err,projectdata){
-                    if(err){
-                        console.log(err);
+            var inputs = {"projectid":projectid,"query":"projectname1"}
+            var args = {
+                data:inputs,
+                headers:{"Content-Type" : "application/json"}
+                
+            }
+            client.post(epurl+"qualityCenter/qcProjectDetails_ICE",args,
+                function (projectdata, response) {
+                    if (response.statusCode != 200 || projectdata.rows == "fail") {
+            // dbConnICE.execute(projectnamequery,function(err,projectdata){
+            //         if(err){
+            //             console.log(err);
+                        console.log("Error occured in getProjectsForUser: fail , projectname1");
                     }else{
                         if(projectdata.rows.length!=0){
                             projectname = projectdata.rows[0].projectname;
@@ -165,9 +188,19 @@ function projectandscenario(projectid,cb){
         },
         scenariodata:function(callback1){
             var scenarioquery = "SELECT testscenarioid,testscenarioname FROM testscenarios where projectid="+projectid
-                dbConnICE.execute(scenarioquery,function(err,scenariorows){
-                if(err){
-                    console.log(err);
+                var inputs = {"projectid":projectid,"query":"scenariodata"}
+                var args = {
+                    data:inputs,
+                    headers:{"Content-Type" : "application/json"}
+                    
+                }
+                client.post(epurl+"qualityCenter/qcProjectDetails_ICE",args,
+                    function (scenariorows, response) {
+                    if (response.statusCode != 200 || scenariorows.rows == "fail") {
+                // dbConnICE.execute(scenarioquery,function(err,scenariorows){
+                // if(err){
+                //     console.log(err);
+                        console.log("Error occured in getProjectsForUser: fail , scenariodata");
                 }else{
                     if(scenariorows.rows.length!=0){
                         flagtocheckifexists = true;
@@ -258,17 +291,68 @@ exports.saveQcDetails_ICE = function(req,res){
         var qcfolderpath = itr.folderpath;
         var qctestcase = itr.testcase;
         var qctestset = itr.testset;
+        //var getprojects = "select projectids from icepermissions where userid="+userid;
+            
         var scenarioquery = "INSERT INTO qualitycenterdetails (testscenarioid,qcdetailsid,qcdomain,qcfolderpath,qcproject,qctestcase,qctestset) VALUES ("+testscenarioid+","+testscenarioid+",'"+qcdomain+"','"+qcfolderpath+"','"+qcproject+"','"+qctestcase+"','"+qctestset+"')";
-                dbConnICE.execute(scenarioquery,function(err,scenariorows){
-                if(err){
-                    console.log(err);
-                    flag = false;   
-                }
+        var inputs = {"testscenarioid":testscenarioid,"qcdetailsid":testscenarioid,"qcdomain":qcdomain,"qcfolderpath":qcfolderpath,"qcproject":qcproject,"qctestcase":qctestcase,"qctestset":qctestset,"query":"saveQcDetails_ICE"}
+            var args = {
+                data:inputs,
+                headers:{"Content-Type" : "application/json"}
+                
+            }
+            client.post(epurl+"qualityCenter/saveQcDetails_ICE",args,
+                function (qcdetailsows, response) {
+                    if (response.statusCode != 200 || qcdetailsows.rows == "fail") {
+                        console.log("Error occured in saveQcDetails_ICE: fail");
+                        flag = false; 
+                    }
+                // dbConnICE.execute(scenarioquery,function(err,scenariorows){
+                // if(err){
+                //     console.log(err);
+                //     flag = false;   
+                // }
                 callback();
             });
     },function(){
         if(flag){
-            res.send("success");
+           
+            try{
+                if(req.cookies['connect.sid'] != undefined)
+                {
+                    var sessionCookie = req.cookies['connect.sid'].split(".");
+                    var sessionToken = sessionCookie[0].split(":");
+                    sessionToken = sessionToken[1];
+                }
+                    if(sessionToken != undefined && req.session.id == sessionToken)
+                    {
+                        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+                        console.log(Object.keys(myserver.allSocketsMap),"<<all people, asking person:",ip);
+                        var mySocket = myserver.allSocketsMap[ip];
+                        if('allSocketsMap' in myserver && ip in myserver.allSocketsMap){
+                                mySocket._events.qcresponse = [];
+                                var qcDetails = {"qcaction":"qcquit"};               						
+                                mySocket.emit("qclogin", qcDetails);
+                                mySocket.on('qcresponse', function (data) {
+                                    res.send("success");
+                                    
+                                });
+                            
+                        }else{
+                            console.log("Socket not Available");
+                            try{
+                                res.send("unavailableLocalServer");
+                            }catch(exception){
+                                console.log(exception);
+                            }
+                        }
+                    }
+                else{
+                    res.send("Invalid Session");
+                }
+            }catch(exception){
+                console.log(exception);
+                res.send("unavailableLocalServer");
+            }
         }else{
             res.send("fail");
         }
@@ -294,9 +378,19 @@ function getQcDetailsForUser(userid,cb){
     async.series({
         getprojectDetails:function(callback1){
             var getprojects = "select projectids from icepermissions where userid="+userid;
-            dbConnICE.execute(getprojects,function(err,projectrows){
-                if(err){
-                    console.log(err);
+            var inputs = {"userid":userid,"query":"getprojectDetails"}
+            var args = {
+                data:inputs,
+                headers:{"Content-Type" : "application/json"}
+                
+            }
+            client.post(epurl+"qualityCenter/qcProjectDetails_ICE",args,
+                function (projectrows, response) {
+                    if (response.statusCode != 200 || projectrows.rows == "fail") {
+            // dbConnICE.execute(getprojects,function(err,projectrows){
+            //     if(err){
+            //         console.log(err);
+                    console.log("Error occured in getQcDetailsForUser: fail , getprojectDetails");
                 }else{
                     if(projectrows.rows.length!=0){
                         flagtocheckifexists = true;
@@ -335,9 +429,18 @@ function qcscenariodetails(projectid,cb){
     async.series({
         scenariodata:function(callback1){
             var scenarioquery = "SELECT testscenarioid,testscenarioname FROM testscenarios where projectid="+projectid
-                dbConnICE.execute(scenarioquery,function(err,scenariorows){
-                if(err){
-                    console.log(err);
+            var inputs = {"projectid":projectid,"query":"scenariodata"}
+            var args = {
+                data:inputs,
+                headers:{"Content-Type" : "application/json"}
+                
+            }
+            client.post(epurl+"qualityCenter/qcProjectDetails_ICE",args,
+                function (scenariorows, response) {
+                if (response.statusCode != 200 || scenariorows.rows == "fail") {
+                // dbConnICE.execute(scenarioquery,function(err,scenariorows){
+                // if(err){
+                //     console.log(err);
                 }else{
                     if(scenariorows.rows.length!=0){
                         flagtocheckifexists = true;
@@ -353,9 +456,19 @@ function qcscenariodetails(projectid,cb){
 		qcdetails:function(callback1){
 			async.forEachSeries(scenarios_list,function(itr,callback2){
 			var qcdetailsquery = "SELECT * FROM qualitycenterdetails where testscenarioid="+itr.testscenarioid;
-			dbConnICE.execute(qcdetailsquery,function(err,qcdetailsows){
-                if(err){
-                    console.log(err);
+            var inputs = {"testscenarioid":itr.testscenarioid,"query":"qcdetails"}
+            var args = {
+                data:inputs,
+                headers:{"Content-Type" : "application/json"}
+                
+            }
+            client.post(epurl+"qualityCenter/viewQcMappedList_ICE",args,
+                function (qcdetailsows, response) {
+                if (response.statusCode != 200 || qcdetailsows.rows == "fail") {
+			// dbConnICE.execute(qcdetailsquery,function(err,qcdetailsows){
+            //     if(err){
+            //         console.log(err);
+                        console.log("Error occured in getQcDetailsForUser: fail , getprojectDetails");
                 }else{
                     if(qcdetailsows.rows.length!=0){
                         flagtocheckifexists = true;
