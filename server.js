5de3e81018b28cc08be1f20ab34220a46d36e430
kavyarathna.s <kavyarathna.s@WSLK13FODC6-040.SLKSOFT.COM>
2017-09-13 15:38:30 +0530

    Fix for #984

--- server.js
+++ server.js
@@ -42,22 +42,22 @@ console.log = function () {
 }
 console.error = console.log;
 
-// if (cluster.isMaster) {
-//     //    cluster.fork();
-//     cluster.fork();
-//     cluster.on('disconnect', function(worker) {
-//         console.log('disconnect!');
-//         // cluster.fork();
-//     });
-//     cluster.on('exit', function(worker) {
+if (cluster.isMaster) {
+    //    cluster.fork();
+    cluster.fork();
+    cluster.on('disconnect', function(worker) {
+        console.log('disconnect!');
+        // cluster.fork();
+    });
+    cluster.on('exit', function(worker) {
 
-//         // Replace the dead worker,
-//         // we're not sentimental
-//         console.log('Let\'s not have Sentiments... Worker %d is killed.', worker.id);
-//         cluster.fork();
-//     });
+        // Replace the dead worker,
+        // we're not sentimental
+        console.log('Let\'s not have Sentiments... Worker %d is killed.', worker.id);
+        cluster.fork();
+    });
 
-// } else {
+} else {
   try {
     var express = require('express');
     var app = express();
@@ -387,7 +387,6 @@ console.error = console.log;
 
   //SOCKET CONNECTION USING SOCKET.IO
     var allClients = [];
-    var allSockets = [];
     var socketMap = {};
     var socketMapUI = {};
     var sokcetMapScheduling={};
@@ -517,4 +516,4 @@ console.error = console.log;
     }, 2)
   }
 
-// }
+}
