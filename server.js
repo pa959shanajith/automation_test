80e147d5efda6d4015c292ab652d28351108bafe
himanshu.jain <himanshu.jain@WSLK13FODC6-051.SLKSOFT.COM>
2017-10-30 18:38:53 +0530

    Updated server.js for neurongraphs

--- server.js
+++ server.js
@@ -410,7 +410,8 @@ if (cluster.isMaster) {
     //NeuronGraphs Plugin Routes
     app.post('/hierarchy_nGraphs2D', neuronGraphs2D.getHierarchy);
     app.post('/getGraph_nGraphs2D', neuronGraphs2D.getGraphData);
-
+    app.post('/getPackData_nGraphs2D', neuronGraphs2D.getPackData);		
+    app.post('/getReportData_nGraphs2D', neuronGraphs2D.getReportData);
     //QC Plugin
     app.post('/loginQCServer_ICE', qc.loginQCServer_ICE);
     app.post('/qcProjectDetails_ICE', qc.qcProjectDetails_ICE);
