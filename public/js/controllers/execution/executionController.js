acdf9584144f863fabd56cc0c57223d9302eb3fa
vinay.niranjan <vinay.niranjan@WSLK13FODC6-055.SLKSOFT.COM>
2018-01-12 19:21:55 +0530

    Nineteen68#1028:In Execution screen select any of the scenario and click on "Save" see selected checkbox gets unselected and all checkbox is getting selected also

--- public/js/controllers/execution/executionController.js
+++ public/js/controllers/execution/executionController.js
@@ -89,8 +89,10 @@ mySPA.controller('executionController',['$scope', '$rootScope', '$http','$timeou
 	$scope.readTestSuite_ICE = function(){
         $('.checkStylebox').attr("disabled", true); 
 		$('#excSaveBtn').attr("disabled", true);
+		blockUI("Loading in Progress. Please Wait");
 		ExecutionService.readTestSuite_ICE(readTestSuite, "execute")
 		.then(function(data) {
+			unblockUI();
 			if(data == "Invalid Session"){
 					$rootScope.redirectPage();
 			 }
