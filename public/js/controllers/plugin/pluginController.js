39daff3213ab85f66a0da386ee9d65b1aa084453
Himanshu Jain <himanshu.jain@slkgroup.com>
2018-03-27 18:57:58 +0530

    Implementation of filter in task list Nineteen68#1546 point 3.
    (cherry picked from commit 0d218daa93df555afcb6a2eff55edd15ccfcc122)

--- public/js/controllers/plugin/pluginController.js
+++ public/js/controllers/plugin/pluginController.js
@@ -5,6 +5,7 @@ mySPA.controller('pluginController',['$scope', '$rootScope', '$window','$http','
 	document.getElementById("currentYear").innerHTML = new Date().getFullYear()
 	var userInfo = JSON.parse(window.localStorage['_UI']);
 	var availablePlugins = userInfo.pluginsInfo;
+	$scope.filterData = {'relval':'Select Release','cycval':'Select Cycle','apptype':{},'tasktype':{}};
 	$("#plugin-container").empty().hide();
 	$("body").css("background", "#fff");
 	if(window.localStorage['navigateScreen'] != "plugin"){
@@ -49,6 +50,7 @@ mySPA.controller('pluginController',['$scope', '$rootScope', '$window','$http','
 						$rootScope.redirectPage();
 					}else{
 						var tasksJson = data;
+						$scope.taskJson = data;
 						//window.localStorage['_TJ'] = angular.toJson(tasksJson);
 						// 	var tasksJson = [{
 						// 	"appType": "Web",
@@ -78,6 +80,11 @@ mySPA.controller('pluginController',['$scope', '$rootScope', '$window','$http','
 						// 	}]
 						// }]
 					 window.localStorage['_TJ'] = angular.toJson(tasksJson);
+					 /*	Build a list of releaseids and cycleids
+					  * Another dict for releaseid and cyclelist out of task json
+					  * List of apptype and tasktype
+					  */
+					    $scope.filterDat = {'releaseids':[],'cycleids':[],'relcycmap':{},'apptypes':[],'tasktypes':['Design','Execution'],'idnamemaprel':{},'idnamemapcyc':{}};
 						$(".plugin-taks-listing").empty().hide()
 						var counter = 1;
 						for(i=0; i<tasksJson.length; i++){
@@ -95,13 +102,44 @@ mySPA.controller('pluginController',['$scope', '$rootScope', '$window','$http','
 									//console.log(i,testSuiteDetails);
 									$(".plugin-taks-listing").append('<div class="panel panel-default"><div class="panel-heading"><div style="margin-top: 9px;min-height: 40px;margin-top: 15px;" href="#collapse'+counter+'"><h4 class="taskNo" style="margin-top: -1px; padding-right: 6px;">'+ counter +'</h4><span class="assignedTask"  data-testsuitedetails='+testSuiteDetails+' data-scenarioflag='+tasksJson[i].scenarioFlag+' data-apptype="'+tasksJson[i].appType+'" data-projectid="'+tasksJson[i].projectId+'"  data-screenid="'+tasksJson[i].screenId+'"  data-screenname="'+tasksJson[i].screenName+'" data-testcaseid="'+tasksJson[i].testCaseId+'" data-testcasename="'+tasksJson[i].testCaseName+'" data-scenarioid="'+tasksJson[i].scenarioId+'" data-taskname="'+tasksJson[i].taskDetails[j].taskName+'" data-taskdes="'+tasksJson[i].taskDetails[j].taskDescription+'" data-tasktype="'+tasksJson[i].taskDetails[j].taskType+'" data-subtask="'+tasksJson[i].taskDetails[j].subTaskType+'" data-subtaskid="'+tasksJson[i].taskDetails[j].subTaskId+'" data-assignedtestscenarioids="'+tasksJson[i].assignedTestScenarioIds+'" data-assignedto="'+tasksJson[i].taskDetails[j].assignedTo+'" data-startdate="'+tasksJson[i].taskDetails[j].startDate+'" data-exenddate="'+tasksJson[i].taskDetails[j].expectedEndDate+'" data-status="'+tasksJson[i].taskDetails[j].status+'" data-versionnumber="'+tasksJson[i].versionnumber+'" data-batchTaskIDs="'+tasksJson[i].taskDetails[j].batchTaskIDs+'" data-releaseid="'+tasksJson[i].releaseid+'" data-cycleid="'+tasksJson[i].cycleid+'" data-reuse="'+tasksJson[i].taskDetails[j].reuse+'" onclick="taskRedirection(this.dataset.testsuitedetails,this.dataset.scenarioflag,this.dataset.assignedtestscenarioids,this.dataset.subtask,this.dataset.subtaskid,this.dataset.screenid,this.dataset.screenname,this.dataset.projectid,this.dataset.taskname,this.dataset.testcaseid,this.dataset.testcasename,this.dataset.apptype,this.dataset.scenarioid,this.dataset.versionnumber,this.dataset.status,this.dataset.batchtaskids,this.dataset.releaseid,this.dataset.cycleid,this.dataset.reuse)">'+tasksJson[i].taskDetails[j].taskName+'</span><!--Addition--><div class="panel-additional-details"><img style="height: 20px;" src="imgs/ic-taskType-blue-plus.png"/><button class="panel-head-tasktype">'+tasksJson[i].taskDetails[j].taskType+'</button></div><!--Addition--></div></div></div>').fadeIn()
 								}
-								counter++
+								counter++;
+								fillFilterValues(tasksJson[i],j);
 							}
-						  }
+
+						}
 						  	//prevent mouseclick before loading tasks
 						  	$("span.toggleClick").removeClass('toggleClick');
 						}
-					
+						PluginService.getNames_ICE($scope.filterDat.releaseids,Array($scope.filterDat.releaseids.length).fill('releases'))
+						.then(function (response) {
+							if(response == "Invalid Session"){
+								$rootScope.redirectPage();
+							}
+							else{
+								response.respnames.forEach(function(name,i){
+									$scope.filterDat.idnamemaprel[response.requestedids[i]] = name;
+								});
+								console.log(response);
+								PluginService.getNames_ICE($scope.filterDat.cycleids,Array($scope.filterDat.cycleids.length).fill('cycles'))
+								.then(function (response) {
+									if(response == "Invalid Session"){
+										$rootScope.redirectPage();
+									}
+									else{
+										response.respnames.forEach(function(name,i){
+											$scope.filterDat.idnamemapcyc[response.requestedids[i]] = name;
+										});
+				
+									/* 
+									*  filtering logic
+									*/
+				
+										
+				
+									}
+								}, function (error) { console.log("Error:::::::::::::", error) })
+							}
+						}, function (error) { console.log("Error:::::::::::::", error) })					
 					//$("#plugin-container").removeClass("inactiveLink");
 				}, function (error) { 
 					console.log("Error:::::::::::::", error);
@@ -268,7 +306,82 @@ mySPA.controller('pluginController',['$scope', '$rootScope', '$window','$http','
 			$window.location.assign("/scheduling")
 		}
 	}
-	
+
+	$scope.showTaskFilterPopup = function(){
+		$('#dialog-taskFilter').modal('show');
+	}
+
+	function passFilterTest(node,tidx){
+		var rflag = false,cflag = false,aflag = false,tflag = false;
+		console.log("filter params: ",$scope.filterData);
+		console.log("node: ",node);
+		if($scope.filterData['relval']=='Select Release' || $scope.filterData['relval']==node.taskDetails[tidx].releaseid) rflag = true;
+		if($scope.filterData['cycval']=='Select Cycle' || $scope.filterData['cycval']==node.taskDetails[tidx].cycleid) cflag = true;
+		if(Object.values($scope.filterData['tasktype']).indexOf(true) == -1 || $scope.filterData.tasktype[node.taskDetails[tidx].taskType]) tflag = true;
+		if(Object.values($scope.filterData['apptype']).indexOf(true) == -1 || $scope.filterData.apptype[node.appType]) aflag = true;		
+		
+		if(rflag && cflag && aflag && tflag) return true;
+		else return false;
+	}
+	$scope.filterTasks = function(){
+		var tasksJson = $scope.taskJson;
+		$('#dialog-taskFilter').modal('hide');
+		console.log("Task Filtered");
+		window.localStorage['_TJ'] = angular.toJson(tasksJson);
+		$(".plugin-taks-listing").empty();
+		var counter = 1;
+		for(i=0; i<tasksJson.length; i++){
+			//console.log("taskJson", tasksJson[i]);
+			for(j=0; j<tasksJson[i].taskDetails.length; j++){
+				//console.log("TASKJSONDETAILS",tasksJson[i].taskDetails);
+				if(tasksJson[i].taskDetails[j].taskType == "Design"){
+					var testSuiteDetails = JSON.stringify(tasksJson[i].testSuiteDetails);
+					if(passFilterTest(tasksJson[i],j))
+						$(".plugin-taks-listing").append('<div class="panel panel-default"><div class="panel-heading"><div style="margin-top: 9px;min-height: 40px;margin-top: 15px;" href="#collapse'+counter+'"><h4 class="taskNo" style="margin-top: -1px; padding-right: 6px;">'+ counter +'</h4><span class="assignedTask" data-testsuitedetails='+testSuiteDetails+' data-scenarioflag='+tasksJson[i].scenarioFlag+' data-apptype="'+tasksJson[i].appType+'" data-projectid="'+tasksJson[i].projectId+'" data-screenid="'+tasksJson[i].screenId+'" data-screenname="'+tasksJson[i].screenName+'" data-testcaseid="'+tasksJson[i].testCaseId+'" data-testcasename="'+tasksJson[i].testCaseName+'" data-scenarioid="'+tasksJson[i].scenarioId+'" data-taskname="'+tasksJson[i].taskDetails[j].taskName+'" data-taskdes="'+tasksJson[i].taskDetails[j].taskDescription+'" data-tasktype="'+tasksJson[i].taskDetails[j].taskType+'" data-subtask="'+tasksJson[i].taskDetails[j].subTaskType+'" data-subtaskid="'+tasksJson[i].taskDetails[j].subTaskId+'"  data-assignedtestscenarioids="'+tasksJson[i].assignedTestScenarioIds+'" data-assignedto="'+tasksJson[i].taskDetails[j].assignedTo+'" data-startdate="'+tasksJson[i].taskDetails[j].startDate+'" data-exenddate="'+tasksJson[i].taskDetails[j].expectedEndDate+'" data-status="'+tasksJson[i].taskDetails[j].status+'" data-versionnumber="'+tasksJson[i].versionnumber+'" data-batchTaskIDs="'+tasksJson[i].taskDetails[j].batchTaskIDs+'" onclick="taskRedirection(this.dataset.testsuitedetails,this.dataset.scenarioflag,this.dataset.assignedtestscenarioids,this.dataset.subtask,this.dataset.subtaskid,this.dataset.screenid,this.dataset.screenname,this.dataset.projectid,this.dataset.taskname,this.dataset.testcaseid,this.dataset.testcasename,this.dataset.apptype,this.dataset.scenarioid,this.dataset.versionnumber,this.dataset.status,this.dataset.batchtaskids)">'+tasksJson[i].taskDetails[j].taskName+'</span><!--Addition--><div class="panel-additional-details"><img style="height: 20px;" src="imgs/ic-taskType-yellow-plus.png"/><button class="panel-head-tasktype">'+tasksJson[i].taskDetails[j].taskType+'</button></div><!--Addition--></div></div></div>').fadeIn()
+				} 
+				else if(tasksJson[i].taskDetails[j].taskType == "Execution"){
+					
+					//console.log("test",tasksJson[i].assignedTestScenarioIds);
+					var testSuiteDetails = JSON.stringify(tasksJson[i].testSuiteDetails);
+					//console.log(i,testSuiteDetails);
+					if(passFilterTest(tasksJson[i],0))
+						$(".plugin-taks-listing").append('<div class="panel panel-default"><div class="panel-heading"><div style="margin-top: 9px;min-height: 40px;margin-top: 15px;" href="#collapse'+counter+'"><h4 class="taskNo" style="margin-top: -1px; padding-right: 6px;">'+ counter +'</h4><span class="assignedTask"  data-testsuitedetails='+testSuiteDetails+' data-scenarioflag='+tasksJson[i].scenarioFlag+' data-apptype="'+tasksJson[i].appType+'" data-projectid="'+tasksJson[i].projectId+'"  data-screenid="'+tasksJson[i].screenId+'"  data-screenname="'+tasksJson[i].screenName+'" data-testcaseid="'+tasksJson[i].testCaseId+'" data-testcasename="'+tasksJson[i].testCaseName+'" data-scenarioid="'+tasksJson[i].scenarioId+'" data-taskname="'+tasksJson[i].taskDetails[j].taskName+'" data-taskdes="'+tasksJson[i].taskDetails[j].taskDescription+'" data-tasktype="'+tasksJson[i].taskDetails[j].taskType+'" data-subtask="'+tasksJson[i].taskDetails[j].subTaskType+'" data-subtaskid="'+tasksJson[i].taskDetails[j].subTaskId+'" data-assignedtestscenarioids="'+tasksJson[i].assignedTestScenarioIds+'" data-assignedto="'+tasksJson[i].taskDetails[j].assignedTo+'" data-startdate="'+tasksJson[i].taskDetails[j].startDate+'" data-exenddate="'+tasksJson[i].taskDetails[j].expectedEndDate+'" data-status="'+tasksJson[i].taskDetails[j].status+'" data-versionnumber="'+tasksJson[i].versionnumber+'" data-batchTaskIDs="'+tasksJson[i].taskDetails[j].batchTaskIDs+'" onclick="taskRedirection(this.dataset.testsuitedetails,this.dataset.scenarioflag,this.dataset.assignedtestscenarioids,this.dataset.subtask,this.dataset.subtaskid,this.dataset.screenid,this.dataset.screenname,this.dataset.projectid,this.dataset.taskname,this.dataset.testcaseid,this.dataset.testcasename,this.dataset.apptype,this.dataset.scenarioid,this.dataset.versionnumber,this.dataset.status,this.dataset.batchtaskids)">'+tasksJson[i].taskDetails[j].taskName+'</span><!--Addition--><div class="panel-additional-details"><img style="height: 20px;" src="imgs/ic-taskType-blue-plus.png"/><button class="panel-head-tasktype">'+tasksJson[i].taskDetails[j].taskType+'</button></div><!--Addition--></div></div></div>').fadeIn()
+				}
+				counter++;
+			}
+		}
+	}
+
+	function fillFilterValues(obj,tidx){
+		/*	Build a list of releaseids and cycleids
+		* Another dict for releaseid and cyclelist out of task json
+		* List of apptype and tasktype
+		*/
+		if($scope.filterDat.releaseids.indexOf(obj.taskDetails[tidx].releaseid) == -1){
+			$scope.filterDat.releaseids.push(obj.taskDetails[tidx].releaseid);
+			$scope.filterDat.relcycmap[obj.taskDetails[tidx].releaseid] = [obj.taskDetails[tidx].cycleid];
+		}
+		else if($scope.filterDat.cycleids.indexOf(obj.taskDetails[tidx].cycleid) == -1){
+			$scope.filterDat.cycleids.push(obj.taskDetails[tidx].cycleid);
+			$scope.filterDat.relcycmap[obj.taskDetails[tidx].releaseid].push(obj.taskDetails[tidx].cycleid);			
+		}
+
+		if($scope.filterDat.apptypes.indexOf(obj.appType)==-1)
+			$scope.filterDat.apptypes.push(obj.appType);
+		
+
+	}
+
+	$('#release-filter-list').change(function(){
+		$('#cycle-filter-list').val('Select Cycle');
+		$scope.filterDat.cycleids.forEach(function(cval,i){
+			$('[value='+cval+']').attr('disabled','disabled');
+		});
+		$scope.filterDat.relcycmap[$('#release-filter-list').val()].forEach(function(cval,i){
+			$('[value='+cval+']').removeAttr("disabled");
+		});
+
+	});
 }]);
 
 //Plugin click event - Creating Scope to define the function and returning back to controller
