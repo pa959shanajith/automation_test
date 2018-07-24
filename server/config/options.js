a519d098dc83f7f17d803ed94f72306cbf7576a7
Ranjan Agrawal <ranjan.agrawal@slkgroup.com>
2018-07-24 14:49:12 +0530

    WebServer cannot access screenshot folder when running as a service. Hence removing the check.

--- server/config/options.js
+++ server/config/options.js
@@ -8,13 +8,6 @@ try {
 	parsed.password = Buffer.from(parsed.password, "base64").toString();
 	parsed.certificate.key = fs.readFileSync(parsed.certificate.key, 'utf-8');
 	parsed.certificate.cert = fs.readFileSync(parsed.certificate.cert, 'utf-8');
-	var screenShotPath = parsed.screenShotPath.default;
-	if (screenShotPath.length > 0) {
-		var scrShot_path_exists = fs.existsSync(screenShotPath);
-		if (!scrShot_path_exists) {
-			throw "Screenshot path \"" + screenShotPath + "\" does not exists or server machine doesn't have sufficient permissions";
-		}
-	}
 } catch (e) {
 	logger.error(e);
 	throw "Please provide valid values in config.json file";
