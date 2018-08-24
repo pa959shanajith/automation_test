32126f05e25a4fe88cc90b0de9f6aa08e7864591
Lohith N S <lohith.s@slksoft.com>
2018-08-24 16:42:49 +0530

    collapsible html reports

--- data/templates/specificReport/helpers.js
+++ data/templates/specificReport/helpers.js
@@ -9,7 +9,13 @@ function getStyle(StepDescription) {
     else return;
 }
 
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
 
+Handlebars.registerHelper('ifnotEquals', function(arg1, arg2, options) {
+    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+});
 function getClass(StepDescription) {
     if(StepDescription.indexOf("Testscriptname") !== -1 || StepDescription.indexOf("TestCase Name") !== -1) return "collapsible-tc demo1";
     else return "rDstepDes tabCont";
