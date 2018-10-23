3c18d7c857f7f00ca4085c882ab80f22d261bd59
vinay.niranjan <vinay.niranjan@WSLK13FODC6-055.SLKSOFT.COM>
2018-10-23 12:20:36 +0530

    e is replaced by event in plugin controller file for search input

--- public/js/controllers/plugin/pluginController.js
+++ public/js/controllers/plugin/pluginController.js
@@ -191,7 +191,7 @@ mySPA.controller('pluginController',['$scope', '$rootScope', '$window','$http','
 	//Search form filter
 	$('.searchInput').keyup(function(event) {
 		filter(this,event); 
-		e.stopImmediatePropagation();
+		event.stopImmediatePropagation();
 	});
 
 	function filter(element,event) {
