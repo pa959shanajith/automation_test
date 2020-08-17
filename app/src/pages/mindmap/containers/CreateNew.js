7a70f4f4e8c2022ea07d0fc88cfbbd2204cd0dd9
Nayak Dheeraj <nayak.dheeraj@slkgroup.com>
2020-08-17 16:42:05 +0530

    Added fontAwesome dependecy and apis

--- app/src/pages/mindmap/containers/CreateNew.js
+++ app/src/pages/mindmap/containers/CreateNew.js
@@ -1,7 +1,9 @@
 import React ,  { Fragment, useEffect } from 'react';
-import {populateProjects} from '../api';
+import {getProjectList , getProjectType} from '../api';
 import { useDispatch, useSelector } from 'react-redux';
+import 'font-awesome/css/font-awesome.min.css';
 import * as actionTypes from '../state/action';
+import '../styles/CreateNew.scss';
 
 /*Component CreateNew
   use: renders create New Mindmap page
@@ -13,14 +15,17 @@ const CreateNew = () => {
   const projectList = useSelector(state=>state.mindmap.projectList)
   useEffect(()=>{
     (async()=>{
-      var res = await populateProjects()
+      var res = await getProjectList()
       var data = parseProjList(res)
+      var projdata = await getProjectType(data[0].id) 
       dispatch({type:actionTypes.UPDATE_PROJECTLIST,payload:data})
     })()
   },[])
   return (
     <Fragment>
         <div>{projectList.length}</div>
+        <div className='create__tool-bar'>
+        </div>
     </Fragment>
   );
 }
