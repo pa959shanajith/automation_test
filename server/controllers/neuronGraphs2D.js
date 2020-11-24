e38540a3c8a4393c4a3ec4cf3a034b5850fd0852
Bharath Kumar H S <bharath.h@slkgroup.com>
2020-11-24 09:15:04 +0000

    resolved code standard issues

--- server/controllers/neuronGraphs2D.js
+++ server/controllers/neuronGraphs2D.js
@@ -4,44 +4,67 @@ var client = new Client();
 var epurl = process.env.DAS_URL;
 var utils = require('../lib/utils');
 
-exports.getGraphData = function(req, res){
-	logger.info("Inside UI service: getGraphData");
-	try{
-		if (utils.isSessionActive(req)) {
-			var inputs={
-				"user_id":req.session.userid
-			}
-			var args = {
-				data: inputs,
-				headers: {
-					"Content-Type": "application/json"
-				}
-			};
-			client.post(epurl + "/neurongraphs/getData", args,
-				function (result, response) {
-				if(response.statusCode != 200 || result.rows == "fail"){
-					//console.log("Status:",status,"\nResponse: ",result);
-					res.status(response.statusCode).send("Error while generating Graph!");
-				}
-				else{
-					if(result.nodes.length==0) res.status(response.statusCode).send({"err":true,"ecode":"DB_NOT_FOUND","msg":"Neuron Graphs DB not found!"});
-					else{
-						var rootIndex=0;
-						var nodeTypes={"DOMAINS_NG":"Domain","PROJECTS_NG":"Project","RELEASES_NG":"Release","CYCLES_NG":"Cycle","TESTSUITES_NG":"TestSuite","TESTSCENARIOS_NG":"TestScenario","TESTCASES_NG":"TestCase","SCREENS_NG":"Screen"};
-						pData={"nodes":result.nodes,"links":result.links,"type":nodeTypes,"root":rootIndex};
-						res.status(response.statusCode).send(pData);
-					}
-				}
-			});
-		}
-		else{
-			res.send({"err":true,"ecode":"INVALID_SESSION","msg":"Your session has been expired. Please login again"});
-		}
-	}
-	catch(exception){
-		logger.info("Exception in the service getGraphData: ", exception);
-		res.send({"err":true,"ecode":"FAIL","msg":"Internal Error! Please contact admin"});
-	}
+exports.getGraphData = function(req, res) {
+    logger.info("Inside UI service: getGraphData");
+    try {
+        if (utils.isSessionActive(req)) {
+            var inputs = {
+                "user_id": req.session.userid
+            }
+            var args = {
+                data: inputs,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
+            client.post(epurl + "/neurongraphs/getData", args,
+                function(result, response) {
+                    if (response.statusCode != 200 || result.rows == "fail") {
+                        //console.log("Status:",status,"\nResponse: ",result);
+                        res.status(response.statusCode).send("Error while generating Graph!");
+                    } else {
+                        if (result.nodes.length == 0) res.status(response.statusCode).send({
+                            "err": true,
+                            "ecode": "DB_NOT_FOUND",
+                            "msg": "Neuron Graphs DB not found!"
+                        });
+                        else {
+                            var rootIndex = 0;
+                            var nodeTypes = {
+                                "DOMAINS_NG": "Domain",
+                                "PROJECTS_NG": "Project",
+                                "RELEASES_NG": "Release",
+                                "CYCLES_NG": "Cycle",
+                                "TESTSUITES_NG": "TestSuite",
+                                "TESTSCENARIOS_NG": "TestScenario",
+                                "TESTCASES_NG": "TestCase",
+                                "SCREENS_NG": "Screen"
+                            };
+                            pData = {
+                                "nodes": result.nodes,
+                                "links": result.links,
+                                "type": nodeTypes,
+                                "root": rootIndex
+                            };
+                            res.status(response.statusCode).send(pData);
+                        }
+                    }
+                });
+        } else {
+            res.send({
+                "err": true,
+                "ecode": "INVALID_SESSION",
+                "msg": "Your session has been expired. Please login again"
+            });
+        }
+    } catch (exception) {
+        logger.info("Exception in the service getGraphData: ", exception);
+        res.send({
+            "err": true,
+            "ecode": "FAIL",
+            "msg": "Internal Error! Please contact admin"
+        });
+    }
 };
 
 exports.getReportNG = function(req, res) {
@@ -50,7 +73,7 @@ exports.getReportNG = function(req, res) {
         if (utils.isSessionActive(req)) {
             var suiteID = req.body.suiteID;
             var reportInfoObj = {};
-           // var reportjson = {};
+            // var reportjson = {};
             var flag = "";
             var finalReport = [];
             var inputs = {
@@ -71,45 +94,41 @@ exports.getReportNG = function(req, res) {
                         logger.error("Error occurred in the service getReportNG - projectsUnderDomain: Failed to get report, executed time and scenarioIds from reports. Error Code : ERRDAS");
                         res.send(flag);
                     } else {
-                        try{
+                        try {
                             //console.log(reportResult);
-                            var pcount=0;
-                            var fcount=0;
-                            var jira=[];
-                            for(var j=0;j<reportResult.rows.length;j++)
-                            { 
-                            var data=reportResult.rows[j];
-                           // var reportvar=data.reports[i];
-                            //var timeTaken=data.reports[i].overallstatus[0].EllapsedTime;
-                            //reportvar.report.rows[i];
-                            //console.log(data);
-                            var tempreport=data.reports;
-                            //var jdata=j.jiraId[i];
-                               
-                            for(var i=0;i<tempreport.length;i++)
-                            {
-                                
-                             var jiraid=tempreport[i].jiraId;
-                             for(var k=0;k<jiraid.length;k++){
-                                var jid=jiraid[k].defectid;
-                                jira.push(jid);
-                             }
-                             
-                             //console.log(jiraid);
+                            var pcount = 0;
+                            var fcount = 0;
+                            var jira = [];
+                            for (var j = 0; j < reportResult.rows.length; j++) {
+                                var data = reportResult.rows[j];
+                                // var reportvar=data.reports[i];
+                                //var timeTaken=data.reports[i].overallstatus[0].EllapsedTime;
+                                //reportvar.report.rows[i];
+                                //console.log(data);
+                                var tempreport = data.reports;
+                                //var jdata=j.jiraId[i];
+
+                                for (var i = 0; i < tempreport.length; i++) {
+
+                                    var jiraid = tempreport[i].jiraId;
+                                    for (var k = 0; k < jiraid.length; k++) {
+                                        var jid = jiraid[k].defectid;
+                                        jira.push(jid);
+                                    }
+
+                                    //console.log(jiraid);
+                                }
+
+                                if (data.status == 'pass') {
+                                    pcount = pcount + 1;
+                                } else {
+                                    fcount = fcount + 1;
+                                }
                             }
 
-                            if(data.status=='pass'){
-                                pcount=pcount+1;
-                            }
-                            else{
-                                fcount=fcount+1;
-                            }
-                        }
-                            //console.log(pcount,fcount);
-                            console.log(data);
-                            var recExec=reportResult.rows.length;
-                            var recentExecutionStatus=reportResult.rows[recExec-1].status;
-                            var totalexecution=pcount+fcount;
+                            var recExec = reportResult.rows.length;
+                            var recentExecutionStatus = reportResult.rows[recExec - 1].status;
+                            var totalexecution = pcount + fcount;
                             reportInfoObj.totalexecution = totalexecution;
                             reportInfoObj.recentExecutionStatus = recentExecutionStatus;
                             reportInfoObj.passcount = pcount;
@@ -120,15 +139,13 @@ exports.getReportNG = function(req, res) {
                             finalReport.push(reportInfoObj);
                             logger.info("Sending reports in the service getReportNG: final function");
                             res.send(finalReport);
-                            }
-						
-						catch (exception) {
+                        } catch (exception) {
                             logger.error("Exception in the service getReportNG: %s", exception);
                             res.send("fail");
-                        }            
+                        }
                     }
                 });
-            } else {
+        } else {
             logger.error("Invalid Session, in the service getReportNG");
             res.send("Invalid Session");
         }
@@ -143,7 +160,7 @@ exports.getReportExecutionStatusNG = function(req, res) {
     try {
         if (utils.isSessionActive(req)) {
             var req_testsuiteId = req.body.suiteId;
-            var startTime, endTime, starttime, endtime,State,suiteID;
+            var startTime, endTime, starttime, endtime, exec_status, suiteID;
             var executionDetailsJSON = [];
             var inputs = {
                 "suiteID": req_testsuiteId
@@ -163,24 +180,22 @@ exports.getReportExecutionStatusNG = function(req, res) {
                             res.send("fail");
                         } else {
                             for (var i = 0; i < executionData.rows.length; i++) {
-								State=executionData.rows[i].status;
-								suiteID=executionData.rows[i].parent;
+                                exec_status = executionData.rows[i].status;
+                                suiteID = executionData.rows[i].parent;
                                 startTime = new Date(executionData.rows[i].starttime);
                                 starttime = startTime.getUTCDate() + "-" + (startTime.getUTCMonth() + 1) + "-" + startTime.getUTCFullYear() + " " + startTime.getUTCHours() + ":" + startTime.getUTCMinutes();
                                 if (executionData.rows[i].endtime === null) endtime = '-';
                                 else {
-                                    //console.log(endTime);
                                     endTime = new Date(executionData.rows[i].endtime);
                                     endtime = endTime.getUTCDate() + "-" + (endTime.getUTCMonth() + 1) + "-" + endTime.getUTCFullYear() + " " + (endTime.getUTCHours()) + ":" + (+endTime.getUTCMinutes());
                                 }
-                                //console.log(endtime);
-                                
+
                                 executionDetailsJSON.push({
                                     execution_id: executionData.rows[i]._id,
                                     start_time: starttime,
                                     end_time: endtime,
-									state:State,
-									suiteid:suiteID
+                                    state: exec_status,
+                                    suiteid: suiteID
                                 });
                             }
                             logger.info("Sending execution details from getReportExecutionStatus_NG: reports/getReportExecutionStatus_NG");
