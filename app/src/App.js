53a50dbd33efe3ace50340844e8a9188ecd58c65
Nayak Dheeraj <nayak.dheeraj@slkgroup.com>
2021-01-28 14:44:59 +0530

    Added socket.io client

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route ,Switch} from "react-router-dom";
+import socketIOClient from "socket.io-client";
+import {Encrypt} from './pages/global';
+import {useDispatch, useSelector} from 'react-redux';
+import {v4 as uuid} from 'uuid';
+import * as actionTypes from './pages/login/state/action';
 import {Provider} from 'react-redux';
 import {store} from './reducer';
 import {ProgressBar} from './pages/global'
@@ -12,6 +17,7 @@ import Mindmap from './pages/mindmap';
 import Scrape from './pages/scrape';
 import Design from './pages/design';
 import Utility from './pages/utility';
+// import Report from './pages/report';
 import Integration from './pages/integration';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
@@ -34,24 +40,40 @@ const App = () => {
   return (
     <Provider store={store}>
       <ProgressBar />
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Base} />
-          <Route path="/login" component={Login} />
-          <Route path="/admin" component={Admin} />
-          <Route path="/mindmap" component={Mindmap} />
-          <Route path="/plugin" component={Plugin} />
-          <Route path ="/scrape" component={Scrape}/>
-          <Route path ="/design" component={Design}/>
-          <Route path ="/utility" component={Utility}/>
-          <Route path = "/integration" component={Integration}/>
-          <Route path ="/execute" component={Execute}/>
-          <Route path ="/scheduling" component={Schedule}/>
-          <Route component={Base} />
-        </Switch>
-      </Router>
+      <RouteApp/>
     </Provider>
   );
+}
+
+const RouteApp = () => {
+  const userInfo = useSelector(state=>state.login.userinfo);
+  const EndPoint = "https://"+window.location.hostname+":8443";
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    var userName = Encrypt.encode((userInfo)?userInfo.username:uuid());
+    var socket = socketIOClient(EndPoint, { forceNew: true, reconnect: true, query: {check: 'notify', key: userName}});
+    socket.emit("key",userName);
+    dispatch({type:actionTypes.SET_SOCKET,payload:socket})
+  },[userInfo])
+  return(
+    <Router>
+    <Switch>
+      <Route exact path="/" component={Base} />
+      <Route path="/login" component={Login} />
+      <Route path="/admin" component={Admin} />
+      <Route path="/mindmap" component={Mindmap} />
+      <Route path="/plugin" component={Plugin} />
+      <Route path ="/scrape" component={Scrape}/>
+      <Route path ="/design" component={Design}/>
+      <Route path ="/utility" component={Utility}/>
+      <Route path = "/integration" component={Integration}/>
+      {/* <Route path = "/reports" component={Report}/> */}
+      <Route path ="/execute" component={Execute}/>
+      <Route path ="/scheduling" component={Schedule}/>
+      <Route component={Base} />
+    </Switch>
+  </Router>
+  )
 }
 
 export default App;
--- app/src/pages/login/state/action.js
+++ app/src/pages/login/state/action.js
@@ -1,2 +1,3 @@
 export const SET_USERINFO = "LOGIN_SET_USERINFO"
 export const SET_SR = "LOGIN_SET_SR"
+export const SET_SOCKET = "LOGIN_SET_SOCKET"
