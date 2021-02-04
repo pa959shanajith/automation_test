5352071aa72e80e6858d08eccd0191c0c299fde0
Aditya K Nayak <aditya.k@slksoft.com>
2021-02-04 18:09:40 +0530

    Test Cases for PluginBox.js PluginSection.js

---
+++ app/src/pages/plugin/tests/dummyData.js
@@ -0,0 +1,63 @@
+export const userInfo={
+    "user_id": "5fb4fbf9f4da702833d7e09e",
+    "username": "user.demo",
+    "email_id": "user.demo@slkgroup.com",
+    "additionalrole": [],
+    "firstname": "user",
+    "lastname": "demo",
+    "role": "5db0022cf87fdec084ae49aa",
+    "taskwflow": false,
+    "token": "720",
+    "dbuser": true,
+    "ldapuser": false,
+    "samluser": false,
+    "openiduser": false,
+    "rolename": "Test Lead",
+    "pluginsInfo": [
+      {
+        "pluginName": "Integration",
+        "pluginValue": true
+      },
+      {
+        "pluginName": "APG",
+        "pluginValue": false
+      },
+      {
+        "pluginName": "Dashboard",
+        "pluginValue": false
+      },
+      {
+        "pluginName": "Mindmap",
+        "pluginValue": true
+      },
+      {
+        "pluginName": "Neuron Graphs",
+        "pluginValue": false
+      },
+      {
+        "pluginName": "Performance Testing",
+        "pluginValue": false
+      },
+      {
+        "pluginName": "Reports",
+        "pluginValue": true
+      },
+      {
+        "pluginName": "Utility",
+        "pluginValue": true
+      },
+      {
+        "pluginName": "Webocular",
+        "pluginValue": false
+      }
+    ],
+    "page": "plugin",
+    "tandc": false
+}
+
+export const dummyPlugin={
+  "pluginName": "Reports",
+  "pluginValue": true,
+  "image":"Reports"
+}
+
