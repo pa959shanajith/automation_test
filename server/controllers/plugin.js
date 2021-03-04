8e5854a0a8aab6e0b9afa7b99df6725c4da3aeee
Divyansh Singh <divyansh.singh@slksoft.com>
2021-03-04 17:53:24 +0530

    Defect - Added return satements to updateAccessibilitySelectionAPI

--- server/controllers/plugin.js
+++ server/controllers/plugin.js
@@ -27,13 +27,13 @@ exports.getProjectIDs = function (req, res) {
 
 exports.updateAccessibilitySelection = async function(req, res){
 	logger.info("Inside UI service: updateAccessibiltySelection");
+	var status = "fail";
 	try{
 		var result = await utils.fetchData(req.body,"/plugins/updateAccessibilitySelection");
-		if(result != 'fail') res.send("success");
-		res.send("fail")
+		if(result != 'fail') status = "success";
+		return res.send(status);
 	}catch(e){
 		logger.error("Error occurred in updateAccessibilitySelection: %s", e);
-		res.send("fail");
 	}
-
+	return res.send("fail");
 }
