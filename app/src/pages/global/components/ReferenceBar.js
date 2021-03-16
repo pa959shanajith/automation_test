645420403d642229bd34977c2bf1e0ca251b1362
vivek.sharma <vivek.sharma@slkgroup.com>
2021-03-16 15:41:59 +0530

    PluginScreen- CSS for Smaller Screen, TaskList ScrollCSS

--- app/src/pages/global/components/ReferenceBar.js
+++ app/src/pages/global/components/ReferenceBar.js
@@ -184,7 +184,7 @@ const ReferenceBar = (props) => {
                             </div>
                             <div className="task_pop__list">
                                 <div id='task_pop_scroll' className="task_pop__overflow">
-                                    <ScrollBar scrollId='task_pop_scroll' trackColor="#46326b" thumbColor="#fff">
+                                    <ScrollBar scrollId='task_pop_scroll' trackColor={'transparent'} thumbColor={'grey'}>
                                         <div className="task_pop__content" id="rb__pop_list">
                                             <TaskContents items={searchValue ? searchItems : taskList} testCaseId={current_task.testCaseId} taskName={current_task.taskName} cycleDict={dataDict.cycleDict} taskJson={tasksJson}/>
                                         </div>
