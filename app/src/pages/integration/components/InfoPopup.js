8073c1344275eeef1e5a11801782d2c4268c7e11
Nayak Dheeraj <nayak.dheeraj@slkgroup.com>
2021-04-28 13:58:11 +0530

    #21860 , #21863 added css and empty data error

--- app/src/pages/integration/components/InfoPopup.js
+++ app/src/pages/integration/components/InfoPopup.js
@@ -1,13 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ModalContainer, ScrollBar} from '../../global';
+import PropTypes from 'prop-types';
 import '../styles/InfoPopup.scss'
 
 /*  component InfoPopup
     props:
         reqDetails - list of dict having details
         setInfo - fn to close popup
+        displayError - fn to show display popup
 */
-const InfoPopup = ({reqDetails,setInfo}) => {
+const InfoPopup = ({reqDetails,setInfo,displayError,screenType}) => {
+    const [err,setErr] = useState(true)
+    useEffect(()=> {
+        if(!reqDetails || reqDetails.length === 0){
+            displayError(reqDetails ? "No requirement details found" : `Please re-map the ${screenType} Test Cases and the AvoAssure scenarios to fetch the requirements`)
+            setInfo(false)
+        }else{
+            setErr(false)
+        }
+    },[])
+    if(err)return null
     return(
         <ModalContainer 
         modalClass = 'modal-md'
@@ -21,41 +33,39 @@ const InfoPopup = ({reqDetails,setInfo}) => {
 
 const Container = ({reqDetails}) =>{
     return(
-        <div className='zphyre-info-box'>
-            {reqDetails.length === 0?
-                <div className='empty-data'>
-                    NO DETAILS FOUND
+        <div data-test='zphyre-info-box' className='zphyre-info-box'>
+            <ScrollBar>
+            {reqDetails.map((e,i)=>
+                <div className='zphyre-info-step' key={'info__'+i}>
+                    <div>
+                        <label className='title'>ID </label>
+                        <label><span>:</span> {e.reqid}</label>
+                    </div>
+                    <div>
+                        <label className='title'>Name </label>
+                        <label><span>:</span> {e.reqname}</label>
+                    </div>
+                    <div>
+                        <label className='title'>Create Date </label>
+                        <label><span>:</span> {e.reqcreationdate}</label>
+                    </div>
+                    <div>
+                        <label className='title'>Description </label>
+                        <label><span>:</span> {e.reqdescription}</label>
+                    </div>
                 </div>
-                :
-                <Fragment>
-                    <ScrollBar>
-                    {reqDetails.map((e,i)=>
-                        <div key={'info__'+i}>
-                            <div>
-                                <label className='title'>ID : </label>
-                                <label>{e.reqid}</label>
-                            </div>
-                            <div>
-                                <label className='title'>name : </label>
-                                <label>{e.reqname}</label>
-                            </div>
-                            <div>
-                                <label className='title'>Create Date : </label>
-                                <label>{e.reqcreationdate}</label>
-                            </div>
-                            <div>
-                                <label className='title'>Description : </label>
-                                <label>{e.reqdescription}</label>
-                            </div>
-                        </div>
-                    )}
-                    </ScrollBar>
-                </Fragment>
-            }
+            )}
+            </ScrollBar>
         </div>
     )
 }
 
 const Footer = ({setInfo}) => <div><button onClick={()=>setInfo(false)}>Ok</button></div>
+
+InfoPopup.propTypes={
+    reqDetails:PropTypes.arrayOf(PropTypes.object),
+    setInfo:PropTypes.func,
+    displayError:PropTypes.func
+}
 
 export default InfoPopup;
