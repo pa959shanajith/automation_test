87854084a44dc6ce6ab44d59f424858d534b72cc
Sushanth Gupta <sushanth.gupta1@slksoft.com>
2022-10-13 13:53:52 +0530

    To remove Rows and pds,view link from the respons.

--- server/lib/execution/executionQueue.js
+++ server/lib/execution/executionQueue.js
@@ -13,6 +13,7 @@ const { timestamp } = require('winston/lib/winston/common');
 const { info } = require('winston');
 const { update } = require('../../notifications');
 const suitFunctions = require('../../controllers/suite');
+const reportFunctions = require('../../controllers/report')
 module.exports.Execution_Queue = class Execution_Queue {
     /*
         this.queue_list: main execution queue, it stores all the queue's corresponding to pools
@@ -31,7 +32,7 @@ module.exports.Execution_Queue = class Execution_Queue {
     static poolname_ice_map = {}
     static res_map = {}
     static key_list = {}
-    static task_list = []
+    static synchronous_execution_report = {}
     static agent_list = {}
     // executon queue initialisation
     static queue_init() {
@@ -624,7 +625,17 @@ module.exports.Execution_Queue = class Execution_Queue {
             return response;
         }
         synchronousFlag = await this.checkForCompletion(req.body.key,gettingTestSuiteIds.executionData.executionListId);
-        if(synchronousFlag) response['status'] = "pass";
+        let synchronous_report = await cache.get('synchronous_report');
+        // console.log(synchronous_report);
+        let responseFromGetReportApi = [];
+        for(let executions of synchronous_report[newExecutionListId]) {
+            const data = await reportFunctions.getDevopsReport_API({
+                'body':executions,
+                'req': req
+            });
+            responseFromGetReportApi.push(data);
+        }
+        if(synchronousFlag) response['status'] = responseFromGetReportApi;
 
         } catch (error) {
             console.info(error);
@@ -783,7 +794,23 @@ module.exports.Execution_Queue = class Execution_Queue {
                     for (let testSuite of executionList){
                         moduleIndex++;
                         if(testSuite.moduleid == resultData.testsuiteId){
-                            this.key_list[resultData.configkey][listIndex][moduleIndex]['status'] = 'COMPLETED'
+                            this.key_list[resultData.configkey][listIndex][moduleIndex]['status'] = 'COMPLETED';
+                            
+                            //Adding details for synchronous report
+                            if(resultData.execReq.executiontype != 'asynchronous') {
+                                if(!(resultData['executionListId'] in this.synchronous_execution_report))
+                                    this.synchronous_execution_report[[resultData['executionListId']]] = [];
+
+                                this.synchronous_execution_report[resultData['executionListId']].push({
+                                    "execution_data": {
+                                        "executionId": resultData.executionId,
+                                        "scenarioIds": resultData.execReq.suitedetails[0].scenarioIds
+                                    }
+                                })
+                                await cache.set("synchronous_report", this.synchronous_execution_report);
+                                let synchronous_report = await cache.get('synchronous_report');
+                                console.log(synchronous_report);
+                            }
                             await cache.set("execution_list", this.key_list);
                         }
                         statusCount+=(testSuite.status == 'COMPLETED');
