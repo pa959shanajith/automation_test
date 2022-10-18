15dc615ef70f7b7a3ad1f64a22036c31a67934e4
Ravi Dwivedi <ravi.dwivedi@slkgroup.com>
2022-10-18 09:52:03 +0000

    Updated index.js

--- app/src/pages/plugin/index.js
+++ app/src/pages/plugin/index.js
@@ -4,9 +4,10 @@ import { useDispatch } from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import * as actions from './state/action';
 import { SetProgressBar, RedirectPage } from '../global';
+import ServiceBell from "@servicebell/widget";
 
 const Plugin = () => {
-
+    ServiceBell("init", "07e1c4e7d40744869cc8cca1ba485f2c");
     const history = useHistory();
     const dispatch = useDispatch();
     
