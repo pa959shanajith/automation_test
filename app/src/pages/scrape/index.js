59e9b4a589f6092c01d6fd529d7d866c5d751f9e
Puneet Asnani <puneet.asnani1@slkgroup.com>
2022-11-04 13:07:30 +0000

    Updated index.js

--- app/src/pages/scrape/index.js
+++ app/src/pages/scrape/index.js
@@ -2,16 +2,10 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { RedirectPage } from '../global';
 import ScrapeScreen from './containers/ScrapeScreen';
-import ServiceBell from "@servicebell/widget";
-import { useSelector} from 'react-redux';
 export let history;
 
 
 const Scrape = ()=>{
-    const userInfo = useSelector(state=>state.login.userinfo);
-    if(userInfo.isTrial){
-        ServiceBell("init", "07e1c4e7d40744869cc8cca1ba485f2c");
-    }
     history = useHistory();
 
     useEffect(()=>{
@@ -19,7 +13,6 @@ const Scrape = ()=>{
             RedirectPage(history, { reason: "screenMismatch" });
         }
     }, []);
-
 
     return (
         <ScrapeScreen/>
