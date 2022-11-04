0ed895905710d019f9d5d16057351fbfa9183ffb
Puneet Asnani <puneet.asnani1@slkgroup.com>
2022-11-04 13:04:13 +0000

    Updated index.js

--- app/src/pages/integration/index.js
+++ app/src/pages/integration/index.js
@@ -1,19 +1,14 @@
 import React, { useEffect } from 'react';
 import Integrations from './containers/IntegrationHome';
-import { useSelector,useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { SetProgressBar, RedirectPage } from '../global';
-import ServiceBell from "@servicebell/widget";
 export var history
 
 
 
 
 const Integration = ()=>{
-    const userInfo = useSelector(state=>state.login.userinfo);
-    if(userInfo.isTrial){
-        ServiceBell("init", "07e1c4e7d40744869cc8cca1ba485f2c");
-    }
     history =  useHistory()
     const dispatch = useDispatch();
     useEffect(()=>{
