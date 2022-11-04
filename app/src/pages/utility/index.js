f18d20e5bb508e07de2208c6d455c4edd45fecb9
Ravi Dwivedi <ravi.dwivedi@slkgroup.com>
2022-11-04 10:58:48 +0000

    Updated index.js

--- app/src/pages/utility/index.js
+++ app/src/pages/utility/index.js
@@ -1,6 +1,6 @@
 import React ,{useEffect} from 'react';
 import Utilities from './containers/UtilityHome';
-import { useDispatch } from 'react-redux';
+import {useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { SetProgressBar, RedirectPage } from '../global';
 import ServiceBell from "@servicebell/widget";
@@ -9,7 +9,10 @@ export var history;
 
 
 const Utility = ()=>{
-    ServiceBell("init", "07e1c4e7d40744869cc8cca1ba485f2c");
+    const userInfo = useSelector(state=>state.login.userinfo);
+    if(userInfo.isTrial){
+        ServiceBell("init", "07e1c4e7d40744869cc8cca1ba485f2c");
+    }
     history =  useHistory()
     const dispatch = useDispatch();
     useEffect(()=>{
