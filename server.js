cc9296f9af957bc1bddb47769a22ca1e554093bb
Rakesh Shivsharanppa Solapure <rakesh.solapure@avoautomation.com>
2023-06-14 17:00:56 +0530

    #37141: adding api for getting nested ProjectMindmapScenario

--- server.js
+++ server.js
@@ -234,7 +234,8 @@ if (cluster.isMaster) {
 
 		var suite = require('./server/controllers/suite');
 		var report = require('./server/controllers/report');
-    var plugin = require('./server/controllers/plugin');
+    	var plugin = require('./server/controllers/plugin');
+		var devOps = require('./server/controllers/devOps');
 
 		// No CSRF token
 		app.post('/ExecuteTestSuite_ICE_SVN', suite.ExecuteTestSuite_ICE_API);
@@ -248,6 +249,7 @@ if (cluster.isMaster) {
 		app.post('/getAgentTask',suite.getAgentTask);
 		app.post('/setExecStatus',suite.setExecStatus);
 		app.post('/getGeniusData',plugin.getGeniusData);
+		app.post('/getProjectsMMTS', devOps.getProjectsMMTS);
 		app.use(csrf({
 			cookie: true
 		}));
