9513002b4f64138738ea26e5e0493a0b2143142b
ravi k <kadaru.ravali@avoautomation.com>
2023-06-29 22:28:30 +0530

    git export and import fixes

--- server.js
+++ server.js
@@ -496,6 +496,7 @@ if (cluster.isMaster) {
 		app.post('/exportToMMSkel', auth.protect, mindmap.exportToMMSkel);
 		app.post('/jsonToMindmap', auth.protect, mindmap.jsonToMindmap);
 		app.post('/singleExcelToMindmap', auth.protect, mindmap.singleExcelToMindmap);
+		app.post('/checkExportName', auth.protect, mindmap.checkExportName);
 		//Login Routes
 		app.post('/checkUser', authlib.checkUser);
 		app.post('/validateUserState', authlib.validateUserState);
