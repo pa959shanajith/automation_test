3ceb82e7c1e38a37fa14a09f0af231a5daa0b878
Krishna-747 <krishnachaitanya125@gmail.com>
2023-07-11 10:41:59 +0530

    reports screen mock API integration

--- app/src/pages/report/components/Profile.js
+++ app/src/pages/report/components/Profile.js
@@ -1,62 +1,205 @@
 import { useState } from "react";
 import { DataTable } from "primereact/datatable";
+import HSBar from "react-horizontal-stacked-bar-chart";
 import { Column } from "primereact/column";
-import { Breadcrumbs, Link } from '@mui/material';
+import { Breadcrumbs, Link } from "@mui/material";
 import AvoInput from "../../../globalComponents/AvoInput";
 import "./Profile.scss";
+import { Badge } from "primereact/badge";
+import { Tree } from "primereact/tree";
+import { reportsBar, reportsData } from "../../utility/mockData";
 
 const Profile = () => {
   const [searchScenario, setSearchScenario] = useState("");
-  const products = [
-    {
-      id: "1000",
-      name: "Add Execution No: E1",
-      dateTime: "22 Aug 2022, 12:54:15 PM",
-      browser: "chrome",
-      module: 100,
-      testScenario: 8,
-    },
-    {
-      id: "1000",
-      name: "Check Execution No: E1",
-      dateTime: "22 Aug 2022, 12:54:15 PM",
-      browser: "chrome",
-      module: 100,
-      testScenario: 8,
-    },
-    {
-      id: "1000",
-      name: "Remove Execution No: E2",
-      dateTime: "22 Aug 2022, 12:54:15 PM",
-      browser: "chrome",
-      module: 100,
-      testScenario: 8,
-    },
-    {
-      id: "1000",
-      name: "Test Execution No: E3",
-      dateTime: "22 Aug 2022, 12:54:15 PM",
-      browser: "chrome",
-      module: 100,
-      testScenario: 8,
-    },
-    {
-      id: "1000",
-      name: "Final Execution No: E4",
-      dateTime: "22 Aug 2022, 12:54:15 PM",
-      browser: "chrome",
-      module: 100,
-      testScenario: 8,
-    },
-  ];
-
-  const columns = [
-    { field: "name", header: "Name" },
+  const [tableColumns, setTableColumns] = useState([
+    { field: "name", header: "Execution" },
+    { field: "status", header: "" },
     { field: "dateTime", header: "Date and Time" },
-    { field: "browser", header: "Browser(s)" },
-    { field: "module", header: "Module(s)" },
-    { field: "testScenario", header: "Test scenarios(s)" },
-  ];
+    { field: "module", header: "Test Suite(s)" },
+    { field: "testCases", header: "Test Case(s)" },
+  ]);
+  const [modules, setModules] = useState([
+    [{
+      key: "0",
+      label: (
+        <HSBar
+          showTextIn
+          data={[
+            { value: 90, description: "90/100 completed", color: "#3b82f6" },
+            { value: 100, description: "100/100 executed", color: "#22c55e" },
+            { value: 80, description: "80/100 passed", color: "#f59e0b" },
+          ]}
+        />
+      ),
+      data: "Documents Folder",
+      children: [
+        {
+          key: "0-0",
+          label: "Testsuite 1",
+          data: "Work Folder",
+          icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+          key: "0-1",
+          label: "Testsuite 2",
+          data: "Home Folder",
+          icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+          key: "0-2",
+          label: "Testsuite 3",
+          data: "Home Folder",
+          icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+          key: "0-3",
+          label: "Testsuite 4",
+          data: "Home Folder",
+        },
+      ],
+    }],
+    [{
+      key: "0",
+      label: (
+        <HSBar
+          showTextIn
+          data={[
+            { value: 70, description: "70/100 completed", color: "#3b82f6" },
+            { value: 100, description: "100/100 executed", color: "#22c55e" },
+            { value: 100, description: "100/100 passed", color: "#f59e0b" },
+          ]}
+        />
+      ),
+      data: "Documents Folder",
+      children: [
+        {
+          key: "0-0",
+          label: "Testsuite 1",
+          data: "Work Folder",
+          icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+          key: "0-1",
+          label: "Testsuite 2",
+          data: "Home Folder",
+          icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+          key: "0-2",
+          label: "Testsuite 3",
+          data: "Home Folder",
+          icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+          key: "0-3",
+          label: "Testsuite 4",
+          data: "Home Folder",
+        },
+      ],
+    }],
+    [{
+      key: "0",
+      label: (
+        <HSBar
+          showTextIn
+          data={[
+            { value: 100, description: "100/100 completed", color: "#3b82f6" },
+            { value: 100, description: "100/100 executed", color: "#22c55e" },
+            { value: 70, description: "70/100 passed", color: "#f59e0b" },
+          ]}
+        />
+      ),
+      data: "Documents Folder",
+      children: [
+        {
+          key: "0-0",
+          label: "Testsuite 1",
+          data: "Work Folder",
+          icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+          key: "0-1",
+          label: "Testsuite 2",
+          data: "Home Folder",
+          icon: 'pi pi-fw pi-plus',
+        },
+        {
+          key: "0-2",
+          label: "Testsuite 3",
+          data: "Home Folder",
+          icon: 'pi pi-fw pi-plus',
+        },
+        {
+          key: "0-3",
+          label: "Testsuite 4",
+          data: "Home Folder",
+        },
+      ],
+    }],
+    [{
+      key: "0",
+      label: (
+        <HSBar
+          showTextIn
+          data={[
+            { value: 50, description: "50/100 completed", color: "#3b82f6" },
+            { value: 80, description: "80/100 executed", color: "#22c55e" },
+            { value: 60, description: "60/100 passed", color: "#f59e0b" },
+          ]}
+        />
+      ),
+      data: "Documents Folder",
+      children: [
+        {
+          key: "0-0",
+          label: "Testsuite 1",
+          data: "Work Folder",
+          icon: 'pi pi-fw pi-plus',
+        },
+        {
+          key: "0-1",
+          label: "Testsuite 2",
+          data: "Home Folder",
+          icon: 'pi pi-fw pi-plus',
+        },
+        {
+          key: "0-2",
+          label: "Testsuite 3",
+          data: "Home Folder",
+          icon: 'pi pi-fw pi-plus',
+        },
+        {
+          key: "0-3",
+          label: "Testsuite 4",
+          data: "Home Folder",
+        },
+      ],
+    }]
+  ]);
+
+  const checkStatus = (statusArr) => {
+    let statusVal;
+    if(statusArr.includes('Fail')) statusVal = "Fail";
+    else if(statusArr.includes('Inprogress')) statusVal = "In progress";
+    else if(statusArr.includes('Queued')) statusVal = "Queued";
+    else statusVal = "Completed"
+    return statusVal;
+  };
+
+  const products = reportsData.map((el, ind) => ({
+    ...el,
+    id: el._id,
+    key: ind.toString(),
+    name: `Execution ${ind + 1}`,
+    dateTime: el.startDate,
+    status: checkStatus(el.modSattus),
+    testSuites: el.modSattus.reduce((ac, cv) => (ac[cv] = ac[cv] + 1 || 1, ac), {}),
+    testCases: el.scestatus.reduce((ac, cv) => (ac[cv] = ac[cv] + 1 || 1, ac), {}),
+  }))
+  
+  const onTestSuiteClick = (getRow) => {
+    console.log(getRow);
+  };
 
   const tableHeader = () => {
     return (
@@ -78,6 +221,81 @@ const Profile = () => {
     );
   };
 
+  const statusBodyTemplate = (rowData) => {
+    let statusSeverity = {
+      Scheduled: "info",
+      Completed: "success",
+      ["In Progress"]: "warning",
+    };
+    return (
+      <Badge
+        value={rowData?.status}
+        severity={statusSeverity[rowData?.status]}
+      ></Badge>
+    );
+  };
+
+  const moduleBodyTemplate = (e) => {
+    let treeArr = [{
+      key: e.key,
+      label: (
+        <HSBar
+          showTextIn
+          data={Object.keys(e.testCases).map((item, ind) => ({
+            value: e.testCases[item], description: `${e.testCases[item]} ${item}`, color: reportsBar[item]
+          }))}
+        />
+      ),
+      data: e.name,
+      children: [
+        {
+            key: '0-0',
+            label: 'Test Case 1',
+            data: 'Work Folder',
+            icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+            key: '0-1',
+            label: 'Test Case 2',
+            data: 'Home Folder',
+            icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+            key: '0-2',
+            label: 'Test Case 3',
+            data: 'Home Folder',
+            icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+            key: '0-3',
+            label: 'Test Case 4',
+            data: 'Home Folder',
+            icon: 'pi pi-fw pi-angle-right',
+        },
+        {
+            key: '0-4',
+            label: 'Test Case 5',
+            data: 'Home Folder',
+            icon: 'pi pi-fw pi-angle-right',
+        }
+    ]
+    }];
+    return <Tree value={treeArr} onNodeClick={(e) => onTestSuiteClick(e)} className="modules_tree" />;
+  };
+
+  const testCaseBodyTemplate = (e) => {
+    return <div className="grid">
+      <div className="col-12">
+      <HSBar
+          showTextIn
+          data={Object.keys(e.testSuites).map((item, ind) => ({
+            value: e.testSuites[item], description: `${e.testSuites[item]} ${item}`, color: reportsBar[item]
+          }))}
+        />
+      </div>
+    </div>
+  };
+
   return (
     <div className="profile_container">
       <Breadcrumbs>
@@ -87,21 +305,24 @@ const Profile = () => {
       </Breadcrumbs>
       <DataTable
         value={products}
-        tableStyle={{ minWidth: "50rem"}}
+        tableStyle={{ minWidth: "50rem" }}
         header={tableHeader}
         globalFilter={searchScenario}
       >
-        {columns.map((col, i) => (
+        {tableColumns.map((col) => (
           <Column
             key={col.field}
             field={col.field}
             header={col.header}
-            {...(col.field === "testScenario"
+            {...(col.field === "testCases"
               ? { filter: true, filterPlaceholder: "Search by name" }
               : {})}
             {...(col.field === "name" || col.field === "dateTime"
               ? { sortable: true }
               : {})}
+            {...(col.field === "status" ? { body: statusBodyTemplate } : {})}
+            {...(col.field === "module" ? { body: moduleBodyTemplate } : {})}
+            {...(col.field === "testCases" ? { body: testCaseBodyTemplate } : {})}
           />
         ))}
       </DataTable>
