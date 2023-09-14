271a4e2d6f452d0f568eb9296a76dbf2d963e389
Shruthi D A <shruthi.d@avoautomation.com>
2023-09-14 10:09:11 +0530

    "admin create user button is missing"

--- app/src/pages/admin/components/AdminSidePanel.js
+++ app/src/pages/admin/components/AdminSidePanel.js
@@ -7,7 +7,7 @@ import '../styles/AdminSidePanel.scss'
 import { useDispatch, useSelector } from 'react-redux';
 
 const AdminSidePanel = () => {
-    const Currentscreen = useSelector(state => state.admin.screen);
+    const currentScreen = useSelector(state => state.admin.screen);
     const [header, setHeader] = useState('');
     const [screen, setScreen] = useState('');
     const dispatch = useDispatch();
@@ -23,17 +23,17 @@ const AdminSidePanel = () => {
                 <AccordionTab header="Authentication Configuration">
                     <div className='flex flex-column pl-4 sidepanel_admin'>
                         <ul>
-                            <li onClick={() => { setHeader('LDAP Configuration'); setScreen('ldapConf'); }} className={Currentscreen === "ldapConf" ? "active_tab" : "not_active_tab"} >
+                            <li onClick={() => { setHeader('LDAP Configuration'); setScreen('ldapConf'); }} className={currentScreen === "ldapConf" ? "active_tab" : "not_active_tab"} >
                                 <img src="static/imgs/ldap_configuration_icon.svg" alt="LDAP" style={{ marginRight: '0.5rem' }} />
                                 LDAP Configuration
                             </li>
 
-                            <li onClick={() => { setHeader('SAML Configuration'); setScreen('samlConf'); }} className={Currentscreen === "samlConf" ? "active_tab" : "not_active_tab"}>
+                            <li onClick={() => { setHeader('SAML Configuration'); setScreen('samlConf'); }} className={currentScreen === "samlConf" ? "active_tab" : "not_active_tab"}>
                                 <img src="static/imgs/saml_configuration_icon.svg" alt="SAML" style={{ marginRight: '0.5rem' }} />
                                 SAML Configuration
                             </li>
 
-                            <li onClick={() => { setHeader('Open ID Connect Configuration'); setScreen('openIdConf'); }} className={Currentscreen === "openIdConf" ? "active_tab" : "not_active_tab"}>
+                            <li onClick={() => { setHeader('Open ID Connect Configuration'); setScreen('openIdConf'); }} className={currentScreen === "openIdConf" ? "active_tab" : "not_active_tab"}>
                                 <img src="static/imgs/openid_connect_icon.svg" alt="open_id_connect" style={{ marginRight: '0.5rem' }} />
                                 Open ID Connect Configuration
                             </li>
@@ -43,12 +43,12 @@ const AdminSidePanel = () => {
                 <AccordionTab header="User management">
                     <div className='flex flex-column pl-4 sidepanel_admin'>
                         <ul>
-                            <li onClick={() => { setHeader('Users'); setScreen('users'); }} className={Currentscreen === "users" ? "active_tab" : "not_active_tab"}>
+                            <li onClick={() => { setHeader('Users'); setScreen('users'); }} className={currentScreen === "users" ? "active_tab" : "not_active_tab"}>
                                 <img src="static/imgs/users_icon.svg" alt="SVG Image" style={{ marginRight: '0.5rem' }} />
                                 Users
                             </li>
 
-                            <li onClick={() => { setHeader('License'); setScreen('license_details'); }} className={Currentscreen === "license_details" ? "active_tab" : "not_active_tab"}>
+                            <li onClick={() => { setHeader('License'); setScreen('license_details'); }} className={currentScreen === "license_details" ? "active_tab" : "not_active_tab"}>
                                 <img src="static/imgs/license_icon.svg" alt="SVG Image" style={{ marginRight: '0.5rem' }} />
                                 License Details
                             </li>
@@ -58,27 +58,27 @@ const AdminSidePanel = () => {
             </Accordion>
             <div className='flex flex-column pl-2 pr-1 m-1 sidepanel_admin'>
                 <ul>
-                    <li onClick={() => { setHeader('Email Server Configuration'); setScreen('email_server_configuration'); }} className={Currentscreen === "email_server_configuration" ? "active_tab" : "not_active_tab"}>
+                    <li onClick={() => { setHeader('Email Server Configuration'); setScreen('email_server_configuration'); }} className={currentScreen === "email_server_configuration" ? "active_tab" : "not_active_tab"}>
                         <img src="static/imgs/email_server_configuration_icon.svg" alt="SVG Image" style={{ marginRight: '0.5rem' }} />
                         Email Server Configuration
                     </li>
 
-                    <li onClick={() => { setHeader('Manage Agents'); setScreen('manage_agents'); }} className={Currentscreen === "manage_agents" ? "active_tab" : "not_active_tab"}>
+                    <li onClick={() => { setHeader('Manage Agents'); setScreen('manage_agents'); }} className={currentScreen === "manage_agents" ? "active_tab" : "not_active_tab"}>
                         <img src="static/imgs/agent_icon.svg" alt="SVG Image" style={{ marginRight: '0.5rem' }} />
                         Manage Agents
                     </li>
 
-                    <li onClick={() => { setHeader('Grid'); setScreen('grid'); }} className={Currentscreen === "grid" ? "active_tab" : "not_active_tab"}>
+                    <li onClick={() => { setHeader('Grid'); setScreen('grid_configuration'); }} className={currentScreen === "grid_configuration" ? "active_tab" : "not_active_tab"}>
                         <img src="static/imgs/license_icon.svg" alt="SVG Image" style={{ marginRight: '0.5rem' }} />
                         Grids
                     </li>
 
-                    <li onClick={() => { setHeader('Session Management'); setScreen('session_management'); }} className={Currentscreen === "session_management" ? "active_tab" : "not_active_tab"}>
+                    <li onClick={() => { setHeader('Session Management'); setScreen('session_management'); }} className={currentScreen === "session_management" ? "active_tab" : "not_active_tab"}>
                         <img src="static/imgs/users_icon.svg" alt="SVG Image" style={{ marginRight: '0.5rem', width: '2rem' }} />
                         Session Management
                     </li>
 
-                    <li onClick={() => { setHeader('Privileges'); setScreen('privileges'); }} className={Currentscreen === "privileges" ? "active_tab" : "not_active_tab"}>
+                    <li onClick={() => { setHeader('Privileges'); setScreen('privileges'); }} className={currentScreen === "privileges" ? "active_tab" : "not_active_tab"}>
                         <img src="static/imgs/privileges_icon.svg" alt="SVG Image" style={{ marginRight: '0.5rem', width: '1.5rem' }} />
                         Privileges
                     </li>
