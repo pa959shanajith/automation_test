f911484e8e3c8a0351e92d62266e16e86b81ab8d
Ravi Dwivedi <ravi.dwivedi@slkgroup.com>
2023-09-19 11:29:25 +0000

    Updated executionQueue.js

--- server/lib/execution/executionQueue.js
+++ server/lib/execution/executionQueue.js
@@ -611,7 +611,7 @@ module.exports.Execution_Queue = class Execution_Queue {
             
             //New Cache Implementation - deviding into clients
             let cacheData = await executionListCache.gethmap(req.hostname)
-            if(cacheData === null) {
+            if(cacheData === null || Object.keys(cacheData).length === 0 && cacheData.constructor === Object) {
                 this.key_list = {};
             } else {
                 this.key_list = JSON.parse(cacheData['execution_list']);
@@ -733,7 +733,7 @@ module.exports.Execution_Queue = class Execution_Queue {
 
                 //New Cache Implementation - dividing into clients
                 let cacheData = await executionListCache.gethmap(req.hostname)
-                if(cacheData === null) {
+                if(vcacheData === null || Object.keys(cacheData).length === 0 && cacheData.constructor === Object) {
                     this.key_list = {};
                 } else {
                     this.key_list = JSON.parse(cacheData['execution_list']);
@@ -791,7 +791,7 @@ module.exports.Execution_Queue = class Execution_Queue {
                 // let cacheData = await cache.get('execution_list')
                 //New Cache Implementation - dividing into clients
                 let cacheData = await executionListCache.gethmap(req.hostname)
-                if(cacheData === null) {
+                if(cacheData === null || Object.keys(cacheData).length === 0 && cacheData.constructor === Object) {
                     this.key_list = {};
                 } else {
                     this.key_list = JSON.parse(cacheData['execution_list']);
@@ -906,7 +906,7 @@ module.exports.Execution_Queue = class Execution_Queue {
 
                 //New Cache Implementation - deviding into clients
                 let cacheData = await executionListCache.gethmap(req.hostname)
-                if(cacheData === null) {
+                if(cacheData === null || Object.keys(cacheData).length === 0 && cacheData.constructor === Object) {
                     this.key_list = {};
                 } else {
                     this.key_list = JSON.parse(cacheData['execution_list']);
