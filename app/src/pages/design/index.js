35ed63cede13f21e68e7449925fa19f6c464b89e
Arpit Tiwari <arpit.tiwari@avoautomation.com>
2023-10-19 14:24:57 +0530

    Fixed Bug #38752

--- app/src/pages/design/index.js
+++ app/src/pages/design/index.js
@@ -1,6 +1,5 @@
 import React,{ useEffect } from 'react';
 import MindmapHome from './containers/MindmapHome';
-import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { RedirectPage } from '../global';
 export var navigate
@@ -11,13 +10,11 @@ export var navigate
 
 const Design = () => {
   navigate =  useNavigate()
-  const dispatch = useDispatch();
   useEffect(()=>{
     if(window.localStorage['navigateScreen'] !== "design"){
         RedirectPage(navigate, { reason: "screenMismatch" });
     }
-    // SetProgressBar("stop", dispatch);
-  }, [dispatch]);
+  }, []);
   return (
       <MindmapHome/>
   );
