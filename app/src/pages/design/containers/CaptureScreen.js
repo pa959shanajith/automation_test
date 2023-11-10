18ea79c0b1a1619e146173cb89ccb3c1e9bdf827
Vikas Ghaghrani <vikas.ghaghrani@slkgroup.com>
2023-11-10 05:10:14 +0000

    Updated CaptureScreen.js

--- app/src/pages/design/containers/CaptureScreen.js
+++ app/src/pages/design/containers/CaptureScreen.js
@@ -1150,7 +1150,7 @@ const elementIdentifier=()=>{
 }  
 const footerSave = (
     <>
-    {(selectedCapturedElement.length>0 && NameOfAppType.appType == "Web") ?<Button label="Element Identifier Order"onClick={elementIdentifier} ></Button>:null}
+    {(selectedCapturedElement.length>0 && typesOfAppType==="Web") ?<Button label="Element Identifier Order"onClick={elementIdentifier} ></Button>:null}
     {selectedCapturedElement.length>0?<Button label='Delete' style={{position:'absolute',left:'1rem',background:'#D9342B',border:'none'}}onClick={onDelete} ></Button>:null}
     <Button label='Cancel' outlined onClick={()=>props.setVisibleCaptureElement(false)}></Button>
     <Button label='Save' onClick={onSave} disabled={saveDisable}></Button>
