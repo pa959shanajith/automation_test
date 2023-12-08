6fc6a6d50d4d9c1d774b02ed6a88ada6e2e2a6d6
ajith <ajith.adimai@avoautomation.com>
2023-12-08 15:57:46 +0530

    logs added for all API

--- server/controllers/ALM.js
+++ server/controllers/ALM.js
@@ -15,11 +15,11 @@ const uiConfig = require('./../config/options');
 // const socket_io = require("../lib/socket")
 const net = require('net');
 const socket_io = require('socket.io')(httpsServer, {
-	cookie: false,
-	pingInterval: uiConfig.socketio.pingInterval,
-	pingTimeout: uiConfig.socketio.pingTimeout,
-	allowEIO3: true,
-	maxHttpBufferSize: 1e8  // approx. 100 MB, default is 1 MB
+cookie: false,
+pingInterval: uiConfig.socketio.pingInterval,
+pingTimeout: uiConfig.socketio.pingTimeout,
+allowEIO3: true,
+maxHttpBufferSize: 1e8  // approx. 100 MB, default is 1 MB
 });
 
 
@@ -61,19 +61,20 @@ exports.create_ALM_Testcase = async function (req, res) {
  
     logger.info("ALM get testcases service called");
     try {
+      logger.info("request payload : "+ req.query.calmTenantId+', '
+                  + req.query.calmTenantLabel + ', '
+                  +req.query.sutTenant + ', '
+                  + req.query.sutSystemType + ', '
+                  + req.query.sutSystemId +  ', '
+                  + req.query.sutBaseUrl + ', '
+                  + req.query.sutSoftwareVersion + ', '
+                  + req.query.project + ', '
+                  + req.query.scope + ', '
+                  + req.query.countryVersion + ', '
+                  + req.query.processes + ', '
+                  + req.query.processGlobalIds
+                  );
         // calmTenantId/:calmTenantLabel/:sutTenant/:sutSystemType/:sutSystemId/:sutBaseUrl/:sutSoftwareVersion/:project/:scope/:countryVersion/:processes/:processGlobalIds
-        console.log(req.query.calmTenantId);
-        console.log(req.query.calmTenantLabel);
-        console.log(req.query.sutTenant);
-        console.log(req.query.sutSystemType);
-        console.log(req.query.sutSystemId);
-        console.log(req.query.sutBaseUrl);
-        console.log(req.query.sutSoftwareVersion);
-        console.log(req.query.project);
-        console.log(req.query.scope);
-        console.log(req.query.countryVersion);
-        console.log(req.query.processes);
-        console.log(req.query.processGlobalIds);
 
         // var inputs = {
         //     "query": "alm_get_testcases"
@@ -90,27 +91,78 @@ exports.create_ALM_Testcase = async function (req, res) {
         // emit_data['testcaseId'] = result[0].rows || ''
         // socket_io.emit('messageFromServer',req.body);
         logger.info("info : testcases api called");
-        res.status(200).send({
-            "project": "11111111-1111-1111-1111-111111111111",
-            "scope": "f787dc00-0e96-4457-82b5-911e2861f6bf",
-            "count": 42,
-            "testcases": [
+        var send_res = {
+          "project": "11111111-1111-1111-1111-111111111111",
+          "scope": "f787dc00-0e96-4457-82b5-911e2861f6bf",
+          "count": 42,
+          "testcases": [
+            {
+              "id": "GUIDTC1",
+              "name": "Default BD9 Test Case",
+              "description": "Validation of the straightforward BD9 processing for Order type OR",
+              "language": "string",
+              "isCustom": true,
+              "automationVersion": true,
+              "process": "BD9",
+              "processGlobalId": "ERL-619d2c82cbe94af99f762d27af62017e",
+              "releaseCreated": "string",
+              "releaseChanged": "string",
+              "createdBy": "John Doe",
+              "lastModifiedBy": "Jane Doe",
+              "createdAt": "2023-12-06T09:46:29.790Z",
+              "lastModifiedAt": "2023-12-06T09:46:29.790Z",
+              "url": "https://<root_automation_tool_URL>/testcases/<GUIDTC1>",
+              "error": {
+                "errorType": "string",
+                "errorCode": 0,
+                "errorShortMessage": "string",
+                "errorLongMessage": "string",
+                "errorUrl": "string"
+              }
+            }
+          ]
+        }
+        logger.info('response : ' +send_res)
+        res.status(200).send(send_res);
+ 
+    } catch (error) {
+        logger.error('getALM_Testcases Error:', error);
+        res.status(500).json({ code:'500', error: 'error while processing' });
+    }
+  };
+  
+  exports.getALM_Datavariants = async function (req, res) {
+    
+    logger.info("ALM get datavariants service called");
+    try {
+      logger.info("request payload : "+ req.query.calmTenantId+', '
+      + req.query.calmTenantLabel + ', '
+      +req.query.sutTenant + ', '
+      + req.query.sutSystemType + ', '
+      + req.query.sutSystemId +  ', '
+      + req.query.sutBaseUrl + ', '
+      + req.query.sutSoftwareVersion + ', '
+      + req.query.project + ', '
+      + req.query.scope + ', '
+      + req.query.countryVersion + ', '
+      + req.query.testCases + ', '
+      + req.query.Language
+      );
+      var send_res = {
+        "project": "11111111-1111-1111-1111-111111111111",
+        "scope": "f787dc00-0e96-4457-82b5-911e2861f6bf",
+        "testcasesvariants": [
+          {
+            "testCaseId": "GUIDTC1",
+            "variants": [
               {
-                "id": "GUIDTC1",
-                "name": "Default BD9 Test Case",
-                "description": "Validation of the straightforward BD9 processing for Order type OR",
+                "id": "VAR01",
+                "name": "Data Variant 1",
+                "description": "Simple data variant for simple scenario",
                 "language": "string",
-                "isCustom": true,
-                "automationVersion": true,
-                "process": "BD9",
-                "processGlobalId": "ERL-619d2c82cbe94af99f762d27af62017e",
-                "releaseCreated": "string",
-                "releaseChanged": "string",
-                "createdBy": "John Doe",
-                "lastModifiedBy": "Jane Doe",
-                "createdAt": "2023-12-06T09:46:29.790Z",
-                "lastModifiedAt": "2023-12-06T09:46:29.790Z",
-                "url": "https://<root_automation_tool_URL>/testcases/<GUIDTC1>",
+                "lastModifiedBy": "John Doe",
+                "lastModifiedAt": "2023-12-06T09:46:29.782Z",
+                "url": "https://<test_automation_tool_URL/datavariant/<Variant_ID>",
                 "error": {
                   "errorType": "string",
                   "errorCode": 0,
@@ -119,32 +171,17 @@ exports.create_ALM_Testcase = async function (req, res) {
                   "errorUrl": "string"
                 }
               }
-            ]
-          });
- 
-    } catch (error) {
-        logger.error('Error:', error);
-        res.status(500).json({ code:'500', error: 'error while processing' });
-    }
-  };
-  
-  exports.getALM_Datavariants = async function (req, res) {
- 
-    logger.info("ALM get datavariants service called");
-    try {
-        console.log(req.query.calmTenantId);
-        console.log(req.query.calmTenantLabel);
-        console.log(req.query.sutTenant);
-        console.log(req.query.sutSystemType);
-        console.log(req.query.sutSystemId);
-        console.log(req.query.sutBaseUrl);
-        console.log(req.query.sutSoftwareVersion);
-        console.log(req.query.project);
-        console.log(req.query.scope);
-        console.log(req.query.countryVersion);
-        console.log(req.query.testCases);
-        console.log(req.query.Language);
-
+            ],
+            "error": {
+              "errorType": "string",
+              "errorCode": 0,
+              "errorShortMessage": "string",
+              "errorLongMessage": "string",
+              "errorUrl": "string"
+            }
+          }
+        ]
+      }
         // var inputs = {
         //     "query": "alm_get_testcases"
         // }
@@ -160,43 +197,11 @@ exports.create_ALM_Testcase = async function (req, res) {
         // emit_data['testcaseId'] = result[0].rows || ''
         // socket_io.emit('messageFromServer',req.body);
         logger.info("info : getALM_Datavariants api called");
-        res.status(200).send({
-            "project": "11111111-1111-1111-1111-111111111111",
-            "scope": "f787dc00-0e96-4457-82b5-911e2861f6bf",
-            "testcasesvariants": [
-              {
-                "testCaseId": "GUIDTC1",
-                "variants": [
-                  {
-                    "id": "VAR01",
-                    "name": "Data Variant 1",
-                    "description": "Simple data variant for simple scenario",
-                    "language": "string",
-                    "lastModifiedBy": "John Doe",
-                    "lastModifiedAt": "2023-12-06T09:46:29.782Z",
-                    "url": "https://<test_automation_tool_URL/datavariant/<Variant_ID>",
-                    "error": {
-                      "errorType": "string",
-                      "errorCode": 0,
-                      "errorShortMessage": "string",
-                      "errorLongMessage": "string",
-                      "errorUrl": "string"
-                    }
-                  }
-                ],
-                "error": {
-                  "errorType": "string",
-                  "errorCode": 0,
-                  "errorShortMessage": "string",
-                  "errorLongMessage": "string",
-                  "errorUrl": "string"
-                }
-              }
-            ]
-          });
+        logger.info("send response : "+send_res);
+        res.status(200).send(send_res);
  
     } catch (error) {
-        logger.error('Error:', error);
+        logger.error('getALM_Datavariants Error:', error);
         res.status(500).json({ code:'500', error: 'error while processing' });
     }
   };
@@ -205,6 +210,26 @@ exports.create_ALM_Testcase = async function (req, res) {
  
     logger.info("ALM Execute testcase service called");
     try {
+      logger.info("request payload : "+ req.body.project+', '
+      + req.body.scope + ', '
+      +req.body.testcases
+      );
+      var send_res = {'data':[
+        {
+          "testCaseId": "GUIDTC1",
+          "testDataVariantId": "VAR01",
+          "jobUrl": "https://<test_automation_tool_URL/jobmonitor/<jobID>",
+          "jobId": "JOB123",
+          "jobName": "string",
+          "error": {
+            "errorType": "string",
+            "errorCode": 0,
+            "errorShortMessage": "string",
+            "errorLongMessage": "string",
+            "errorUrl": "string"
+          }
+        }
+      ]}
         // var inputs = {
         //     "query": "alm_get_testcases"
         // }
@@ -220,25 +245,11 @@ exports.create_ALM_Testcase = async function (req, res) {
         // emit_data['testcaseId'] = result[0].rows || ''
         // socket_io.emit('messageFromServer',req.body);
         logger.info("info : Execute_Testcase api called");
-        res.status(202).send({'data':[
-            {
-              "testCaseId": "GUIDTC1",
-              "testDataVariantId": "VAR01",
-              "jobUrl": "https://<test_automation_tool_URL/jobmonitor/<jobID>",
-              "jobId": "JOB123",
-              "jobName": "string",
-              "error": {
-                "errorType": "string",
-                "errorCode": 0,
-                "errorShortMessage": "string",
-                "errorLongMessage": "string",
-                "errorUrl": "string"
-              }
-            }
-          ]});
+        logger.info("send response : "+send_res);
+        res.status(202).send(send_res);
  
     } catch (error) {
-        logger.error('Error:', error);
+        logger.error(' Execute_Testcase Error: ', error);
         res.status(500).json({ code:'500', error: 'error while processing' });
     }
   };  
@@ -247,9 +258,28 @@ exports.create_ALM_Testcase = async function (req, res) {
  
     logger.info("ALM Job Status service called");
     try {
-        console.log(req.query.project);
-        console.log(req.query.scope);
-        console.log(req.query.jobIds);
+      logger.info("request payload : "+ req.query.project+', '
+      + req.query.scope + ', '
+      +req.query.jobIds
+      );
+      var send_res = {
+        "project": "11111111-1111-1111-1111-111111111111",
+        "scope": "f787dc00-0e96-4457-82b5-911e2861f6bf",
+        "jobstatuses": [
+          {
+            "id": "JOB123",
+            "status": "running",
+            "percentage": 50,
+            "error": {
+              "errorType": "string",
+              "errorCode": 0,
+              "errorShortMessage": "string",
+              "errorLongMessage": "string",
+              "errorUrl": "string"
+            }
+          }
+        ]
+      }
         // var inputs = {
         //     "query": "alm_get_testcases"
         // }
@@ -264,28 +294,12 @@ exports.create_ALM_Testcase = async function (req, res) {
         // }
         // emit_data['testcaseId'] = result[0].rows || ''
         // socket_io.emit('messageFromServer',req.body);
-        logger.info("info : Job_Status api called , "+req.params.jobids);
-        res.status(200).send({
-            "project": "11111111-1111-1111-1111-111111111111",
-            "scope": "f787dc00-0e96-4457-82b5-911e2861f6bf",
-            "jobstatuses": [
-              {
-                "id": "JOB123",
-                "status": "running",
-                "percentage": 50,
-                "error": {
-                  "errorType": "string",
-                  "errorCode": 0,
-                  "errorShortMessage": "string",
-                  "errorLongMessage": "string",
-                  "errorUrl": "string"
-                }
-              }
-            ]
-          });
+        logger.info("info : Job_Status api called , "+req.query.jobIds);
+        logger.info("send response : "+send_res)
+        res.status(200).send(send_res);
  
     } catch (error) {
-        logger.error('Error:', error);
+        logger.error('Job_Status Error: ', error);
         res.status(500).json({ code:'500', error: 'error while processing' });
     }
   };
@@ -294,18 +308,46 @@ exports.create_ALM_Testcase = async function (req, res) {
  
     logger.info("ALM Execution History service called");
     try {
-        console.log(req.query.calmTenantId);
-        console.log(req.query.calmTenantLabel);
-        console.log(req.query.sutTenant);
-        console.log(req.query.sutSystemType);
-        console.log(req.query.sutSystemId);
-        console.log(req.query.sutBaseUrl);
-        console.log(req.query.sutSoftwareVersion);
-        console.log(req.query.project);
-        console.log(req.query.scope);
-        console.log(req.query.language);
-        console.log(req.query.testCases);
-        console.log(req.query.lastExecutionCount);
+      logger.info("request payload : "+ req.query.calmTenantId+', '
+      + req.query.calmTenantLabel + ', '
+      +req.query.sutTenant + ', '
+      + req.query.sutSystemType + ', '
+      + req.query.sutSystemId +  ', '
+      + req.query.sutBaseUrl + ', '
+      + req.query.sutSoftwareVersion + ', '
+      + req.query.project + ', '
+      + req.query.scope + ', '
+      + req.query.language + ', '
+      + req.query.testCases + ', '
+      + req.query.lastExecutionCount
+      );
+        var send_res = {
+          "project": "11111111-1111-1111-1111-111111111111",
+          "scope": "f787dc00-0e96-4457-82b5-911e2861f6bf",
+          "testcaseshistory": [
+            {
+              "testCaseId": "GUIDTC1",
+              "testDataVariantId": "VAR01",
+              "jobId": "JOB123",
+              "jobName": "string",
+              "jobUrl": "string",
+              "logUrl": "string",
+              "startedAt": "2023-12-06T11:28:41.259Z",
+              "startedBy": "John Doe",
+              "endedAt": "2023-12-06T11:28:41.259Z",
+              "resultStatus": "failed",
+              "message": "Save button not found - Sales order could not be created",
+              "language": "string",
+              "error": {
+                "errorType": "string",
+                "errorCode": 0,
+                "errorShortMessage": "string",
+                "errorLongMessage": "string",
+                "errorUrl": "string"
+              }
+            }
+          ]
+        }
         // var inputs = {
         //     "query": "alm_get_testcases"
         // }
@@ -321,36 +363,11 @@ exports.create_ALM_Testcase = async function (req, res) {
         // emit_data['testcaseId'] = result[0].rows || ''
         // socket_io.emit('messageFromServer',req.body);
         logger.info("info : Execution_History api called , "+req.params.testcaseId);
-        res.status(200).send({
-            "project": "11111111-1111-1111-1111-111111111111",
-            "scope": "f787dc00-0e96-4457-82b5-911e2861f6bf",
-            "testcaseshistory": [
-              {
-                "testCaseId": "GUIDTC1",
-                "testDataVariantId": "VAR01",
-                "jobId": "JOB123",
-                "jobName": "string",
-                "jobUrl": "string",
-                "logUrl": "string",
-                "startedAt": "2023-12-06T11:28:41.259Z",
-                "startedBy": "John Doe",
-                "endedAt": "2023-12-06T11:28:41.259Z",
-                "resultStatus": "failed",
-                "message": "Save button not found - Sales order could not be created",
-                "language": "string",
-                "error": {
-                  "errorType": "string",
-                  "errorCode": 0,
-                  "errorShortMessage": "string",
-                  "errorLongMessage": "string",
-                  "errorUrl": "string"
-                }
-              }
-            ]
-          });
+        logger.info("send response : "+send_res)
+        res.status(200).send(send_res);
  
     } catch (error) {
-        logger.error('Error:', error);
+        logger.error('Execution_History Error: ', error);
         res.status(500).json({ code:'500', error: 'error while processing' });
     }
   };
@@ -359,6 +376,23 @@ exports.create_ALM_Testcase = async function (req, res) {
  
     logger.info("ALM Scope Change service called");
     try {
+      logger.info("request payload : "+ req.query.data);
+      var send_res = { 'data':
+      [
+          {
+            "process": "BD9",
+            "processGlobalId": "ERL-619d2c82cbe94af99f762d27af62017e",
+            "created": true,
+            "error": {
+              "errorType": "string",
+              "errorCode": 0,
+              "errorShortMessage": "string",
+              "errorLongMessage": "string",
+              "errorUrl": "string"
+            }
+          }
+        ]
+        }
         // var inputs = {
         //     "query": "alm_get_testcases"
         // }
@@ -374,25 +408,11 @@ exports.create_ALM_Testcase = async function (req, res) {
         // emit_data['testcaseId'] = result[0].rows || ''
         // socket_io.emit('messageFromServer',req.body);
         logger.info("info : Scope_Changed api called ");
-        res.status(201).send({ 'data':
-        [
-            {
-              "process": "BD9",
-              "processGlobalId": "ERL-619d2c82cbe94af99f762d27af62017e",
-              "created": true,
-              "error": {
-                "errorType": "string",
-                "errorCode": 0,
-                "errorShortMessage": "string",
-                "errorLongMessage": "string",
-                "errorUrl": "string"
-              }
-            }
-          ]
-          });
+        logger.info("send response : "+send_res)
+        res.status(201).send(send_res);
  
     } catch (error) {
-        logger.error('Error:', error);
+        logger.error('Scope_Changed Error: ', error);
         res.status(500).json({ code:'500', error: 'error while processing' });
     }
   };
