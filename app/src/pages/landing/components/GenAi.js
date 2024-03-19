b0132e0bfef480e15752722fb8676f23b181a180
Prashant S Laxmeshwarmath <prashant.laxmeshwarmath@avoautomation.com>
2024-03-19 15:48:08 +0530

    Fixed issue where file name of other projects were showing after uploading

--- app/src/pages/landing/components/GenAi.js
+++ app/src/pages/landing/components/GenAi.js
@@ -75,7 +75,7 @@ const GenAi = () => {
                 //    },
                 // })
                 if (uploadFilesData) {
-                    const filteredData = uploadFilesData.data?.filter((data) => data.project == reduxDefaultselectedProject?.projectName)
+                    const filteredData = uploadFilesData?.data?.filter((data) => data.project == reduxDefaultselectedProject?.projectName)
                     setFileDetails(filteredData);
                     setBadgeValue(Object.keys(filteredData).length);
                 }
@@ -169,15 +169,16 @@ const GenAi = () => {
             } else {
                 try {
                     refreshFileUpload()
-                    const uploadFilesData = await getall_uploadfiles(querystrg)
+                    const uploadFilesData = await getall_uploadfiles(querystrg);
+                    const filteredData = uploadFilesData?.data?.filter((data) => data.project == reduxDefaultselectedProject?.projectName)
+                    setFileDetails(filteredData);
                     // axios.get('/getall_uploadfiles', {
                     //    params: {
                     //       email: email,
                     //    },
                     // })
-                    setFileDetails(uploadFilesData.data);
                     toast.current.show({ severity: 'success', summary: 'Success', detail: 'get uploaded file', life: 3000 });
-                    setBadgeValue(Object.keys(uploadFilesData.data).length);
+                    setBadgeValue(Object.keys(filteredData).length);
                 } catch (uploadFilesError) {
                     toast.current.show({ severity: 'error', summary: 'Error', detail: 'errorMessage', life: 3000 });
                 }
